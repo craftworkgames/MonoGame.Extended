2062d8a786391ff37719423aee98418f3af0882c
Dylan Wilson <dylan@craftworkgames.com>
2015-07-27 22:36:15 +1000

    started working on tiled map loading

---
+++ Source/MonoGame.Extended.Content.Pipeline/TiledMaps/TiledMapImporter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using TiledSharp;
+
+namespace MonoGame.Extended.Content.Pipeline.TiledMaps
+{
+    public class TiledMapFile
+    {
+    }
+
+    [ContentImporter(".tmx", DefaultProcessor = "TiledMapProcessor", DisplayName = "Tiled Map Importer - MonoGame.Extended")]
+    public class TiledMapImporter : ContentImporter<TiledMapFile>
+    {
+        public override TiledMapFile Import(string filename, ContentImporterContext context)
+        {
+            var map = new TmxMap(filename);
+            return new TiledMapFile();
+        }
+    }
+}
