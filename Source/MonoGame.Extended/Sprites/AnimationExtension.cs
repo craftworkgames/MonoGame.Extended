ff4f24681a82a714252a410f6956c18feb2671bf
cra0zy <cra0zy@gmail.com>
2015-09-11 15:06:56 +0200

    Added Animation class

---
+++ Source/MonoGame.Extended/Sprites/AnimationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Extended.Sprites
+{
+    public static class AnimationExtension
+    {
+        public static void Draw(this SpriteBatch spriteBatch, Animation animation)
+        {
+            spriteBatch.Draw(animation._sprites[animation.FrameIndex]);
+        }
+    }
+}
+
