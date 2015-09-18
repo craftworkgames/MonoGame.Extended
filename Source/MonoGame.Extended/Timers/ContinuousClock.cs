5da00258164473c9cdcf9a03447cef80e03f9a15
Andrew Godfroy <andrew.godfroy@killerrin.com>
2015-09-18 15:32:29 -0400

    Added Timers

---
+++ Source/MonoGame.Extended/Timers/ContinuousClock.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoGame.Extended.Timers
+{
+    public class ContinuousClock : IGameComponent
+    {
+        public event EventHandler ReportTimePassed;
+
+        public TimerState TimerState;
+        public double ReportEveryXSeconds;
+
+        public TimeSpan CurrentTime { get; protected set; } = TimeSpan.Zero;
+        public TimeSpan NextReport { get; protected set; } = TimeSpan.Zero;
+
+        public ContinuousClock(double reportEveryXSeconds)
+        {
+            ReportEveryXSeconds = reportEveryXSeconds;
+            Reset();
+        }
+
+        void IGameComponent.Initialize() { Reset(); }
+        public void Reset()
+        {
+            TimerState = TimerState.Stopped;
+            CurrentTime = TimeSpan.Zero;
+            NextReport = CurrentTime + TimeSpan.FromSeconds(ReportEveryXSeconds);
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            if (TimerState == TimerState.Stopped ||
+                TimerState == TimerState.Paused)
+                return;
+
+            CurrentTime += gameTime.ElapsedGameTime;
+
+            if (CurrentTime >= NextReport)
+            {
+                NextReport = CurrentTime + TimeSpan.FromSeconds(ReportEveryXSeconds);
+
+                if (ReportTimePassed != null)
+                    ReportTimePassed(this, null);
+            }
+        }
+    }
+}
