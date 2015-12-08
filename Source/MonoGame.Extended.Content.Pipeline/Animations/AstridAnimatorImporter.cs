f0f1e4a4aee873074bc299daf8b4bc4449b17d0f
Dylan Wilson <dylan@craftworkgames.com>
2015-12-08 18:51:35 +1000

    started work on the astrid animator importer

---
+++ Source/MonoGame.Extended.Content.Pipeline/Animations/AstridAnimatorImporter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace MonoGame.Extended.Content.Pipeline.Animations
+{
+    [ContentImporter(".aa", DefaultProcessor = "AstridAnimatorProcessor", 
+        DisplayName = "Astrid Animator Importer - MonoGame.Extended")]
+    public class AstridAnimatorImporter : ContentImporter<AstridAnimatorFile>
+    {
+        public override AstridAnimatorFile Import(string filename, ContentImporterContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
