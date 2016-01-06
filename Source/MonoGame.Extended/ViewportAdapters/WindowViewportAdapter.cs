df6c0ea2ce668beb509bf67d403d6657de5d383a
cupstudio <gene.cupstid@gmail.com>
2016-01-06 11:25:29 -0600

    Add WindowViewportAdapter.cs - allows for resizing viewport.

---
+++ Source/MonoGame.Extended/ViewportAdapters/WindowViewportAdapter.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonoGame.Extended.ViewportAdapters
+{
+    public class WindowViewportAdapter : ViewportAdapter
+    {
+        public WindowViewportAdapter(GameWindow window, GraphicsDevice graphicsDevice)
+        {
+            GraphicsDevice = graphicsDevice;
+            Window = window;
+            window.ClientSizeChanged += OnClientSizeChanged;
+        }
+
+        protected readonly GameWindow Window;
+        protected GraphicsDevice GraphicsDevice;
+
+        public override int ViewportWidth
+        {
+            get { return Window.ClientBounds.Width; }
+        }
+        public override int ViewportHeight
+        {
+            get { return Window.ClientBounds.Height; }
+        }
+
+        public override int VirtualWidth
+        {
+            get { return Window.ClientBounds.Width; }
+        }
+
+        public override int VirtualHeight
+        {
+            get { return Window.ClientBounds.Height; }
+        }
+
+        public override Matrix GetScaleMatrix()
+        {
+            return Matrix.Identity;
+        }
+
+        private void OnClientSizeChanged(object sender, EventArgs eventArgs)
+        {
+            var x = Window.ClientBounds.Width;
+            var y = Window.ClientBounds.Height;
+
+            GraphicsDevice.Viewport = new Viewport(0, 0, x, y);
+        }
+
+    }
+}
