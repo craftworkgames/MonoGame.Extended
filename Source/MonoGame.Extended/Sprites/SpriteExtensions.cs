943479a1b22355399465204c309e860d0095c990
Dylan Wilson <dylan@craftworkgames.com>
2016-01-12 20:43:44 +1000

    fixed a bug in the camera frustum calculations

--- Source/MonoGame.Extended/Sprites/SpriteExtensions.cs
+++ Source/MonoGame.Extended/Sprites/SpriteExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Animations;
-using MonoGame.Extended.TextureAtlases;
 
 namespace MonoGame.Extended.Sprites
 {
