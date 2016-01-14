71e7b3c2f36131febec7732659427511726b9288
Dylan Wilson <dylan@craftworkgames.com>
2016-01-14 18:07:10 +1000

    started work on screen management

---
+++ Source/MonoGame.Extended/Screens/Screen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended.Screens
+{
+    public abstract class Screen : IDisposable
+    {
+        public virtual void Dispose() { }
+        public virtual void Show() { }
+        public virtual void Hide() { }
+        public virtual void Resize(int width, int height) { }
+        public virtual void Pause() { }
+        public virtual void Resume() { }
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
+    }
+}
