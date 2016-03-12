d509dc17e41bc65b023ab4b3c845dd684f0b9740
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-03-12 14:42:22 -0800

    cleanup

--- Source/MonoGame.Extended.Tests/UnsafeCodeTests.cs
+++ Source/MonoGame.Extended.Tests/UnsafeCodeTests.cs
@@ -7,66 +7,66 @@ namespace MonoGame.Extended.Tests
     [TestFixture]
     public class UnsafeCodeTests
     {
-        // The unsafe keyword allows pointers to be used within the following method:
-        static unsafe void Copy(byte[] src, int srcIndex,
-            byte[] dst, int dstIndex, int count)
+        [Test]
+        public void Unsafe_Copy_Test()
         {
-            if (src == null || srcIndex < 0 ||
-                dst == null || dstIndex < 0 || count < 0)
+            var a = new byte[100];
+            var b = new byte[100];
+            for (var i = 0; i < 100; ++i)
             {
-                throw new ArgumentException();
+                a[i] = (byte)i;
             }
-            int srcLen = src.Length;
-            int dstLen = dst.Length;
-            if (srcLen - srcIndex < count ||
-                dstLen - dstIndex < count)
+            UnsafeCopy(a, 0, b, 0, 100);
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append("The first 10 elements are: ");
+            for (var i = 0; i < 10; ++i)
             {
-                throw new ArgumentException();
+                stringBuilder.Append(b[i] + " ");
             }
 
+            Assert.AreEqual(stringBuilder.ToString(), "The first 10 elements are: 0 1 2 3 4 5 6 7 8 9 ");
+        }
+
+        // The unsafe keyword allows pointers to be used within the following method:
+        private static unsafe void UnsafeCopy(byte[] src, int srcIndex, byte[] dst, int dstIndex, int count)
+        {
+            if (src == null || srcIndex < 0 || dst == null || dstIndex < 0 || count < 0)
+            {
+                throw new ArgumentException();
+            }
+            var srcLen = src.Length;
+            var dstLen = dst.Length;
+            if (srcLen - srcIndex < count || dstLen - dstIndex < count)
+            {
+                throw new ArgumentException();
+            }
 
             // The following fixed statement pins the location of
             // the src and dst objects in memory so that they will
             // not be moved by garbage collection.          
             fixed (byte* pSrc = src, pDst = dst)
             {
-                byte* ps = pSrc;
-                byte* pd = pDst;
+                var ps = pSrc;
+                var pd = pDst;
 
                 // Loop over the count in blocks of 4 bytes, copying an
                 // integer (4 bytes) at a time:
-                for (int n = 0; n < count / 4; n++)
+                for (var n = 0; n < count / 4; n++)
                 {
-                    *((int*)pd) = *((int*)ps);
+                    *(int*)pd = *(int*)ps;
                     pd += 4;
                     ps += 4;
                 }
 
                 // Complete the copy by moving any bytes that weren't
                 // moved in blocks of 4:
-                for (int n = 0; n < count % 4; n++)
+                for (var n = 0; n < count % 4; n++)
                 {
                     *pd = *ps;
                     pd++;
                     ps++;
                 }
             }
-        }
-
-        [Test]
-        public void Unsafe_Copy_Test()
-        {
-            byte[] a = new byte[100];
-            byte[] b = new byte[100];
-            for (int i = 0; i < 100; ++i)
-                a[i] = (byte)i;
-            Copy(a, 0, b, 0, 100);
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append("The first 10 elements are: ");
-            for (int i = 0; i < 10; ++i)
-                stringBuilder.Append(b[i] + " ");
-
-            Assert.AreEqual(stringBuilder.ToString(), "The first 10 elements are: 0 1 2 3 4 5 6 7 8 9 ");
         }
     }
 }
