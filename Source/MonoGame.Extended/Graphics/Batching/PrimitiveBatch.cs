abfa21c6e7d8e61dc295399b80238e0c7b376cf5
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-04-02 12:08:01 -0700

    primitive batch filled in

--- Source/MonoGame.Extended/Graphics/Batching/PrimitiveBatch.cs
+++ Source/MonoGame.Extended/Graphics/Batching/PrimitiveBatch.cs
@@ -6,11 +6,13 @@ namespace MonoGame.Extended.Graphics.Batching
     public class PrimitiveBatch<TVertexType> : IDisposable
         where TVertexType : struct, IVertexType
     {
-        private IBatcher<TVertexType> _batcher; 
+        private IBatchDrawer<TVertexType> _batchDrawer;
+        private IBatchQueuer<TVertexType> _batchQueuer; 
 
-        public PrimitiveBatch(IBatcher<TVertexType> batcher)
+        public PrimitiveBatch(IBatchDrawer<TVertexType> batchDrawer, IBatchQueuer<TVertexType> batchQueuer)
         {
-            _batcher = batcher;
+            _batchDrawer = batchDrawer;
+            _batchQueuer = batchQueuer;
         }
 
         public void Dispose()
@@ -26,32 +28,40 @@ namespace MonoGame.Extended.Graphics.Batching
                 return;
             }
 
-            _batcher.Dispose();
-            _batcher = null;
+            _batchDrawer.Dispose();
+            _batchDrawer = null;
+            _batchQueuer.Dispose();
+            _batchQueuer = null;
         }
 
         public void Begin()
         {
+            _batchQueuer.Begin();
         }
 
         public void End()
         {  
+            _batchQueuer.End();
         }
 
-        public void Draw(PrimitiveType primitiveType, TVertexType vertices, IDrawContext context)
+        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, IDrawContext drawContext)
         { 
+            _batchQueuer.Queue(primitiveType, vertices, 0, vertices.Length, drawContext);
         }
 
-        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, int startVertex, int vertexCount, IDrawContext context)
+        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, int startVertex, int vertexCount, IDrawContext drawContext)
         {
+            _batchQueuer.Queue(primitiveType, vertices, startVertex, vertexCount, drawContext);
         }
 
-        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, short[] indices, IDrawContext context)
+        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, short[] indices, IDrawContext drawContext)
         {
+            _batchQueuer.Queue(primitiveType, vertices, 0, vertices.Length, indices, 0, indices.Length, drawContext);
         }
 
-        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, int startVertex, int vertexCount, short[] indices, int startIndex, int indexCount, IDrawContext context)
+        public void Draw(PrimitiveType primitiveType, TVertexType[] vertices, int startVertex, int vertexCount, short[] indices, int startIndex, int indexCount, IDrawContext drawContext)
         {
+            _batchQueuer.Queue(primitiveType, vertices, startVertex, vertexCount, indices, startIndex, indexCount, drawContext);
         }
     }
 }
