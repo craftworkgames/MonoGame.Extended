ede988abe18b23c3e9a2d996f9dd3a7c17fb8e3d
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-04-06 09:31:54 -0700

    immediate batch queuer tests

---
+++ Source/MonoGame.Extended.Tests/Graphics/Batching/ImmediateBatchQueuerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Graphics;
+using MonoGame.Extended.Graphics.Batching;
+using NUnit.Framework;
+using TestVertex = Microsoft.Xna.Framework.Graphics.VertexPositionColor;
+
+namespace MonoGame.Extended.Tests.Graphics.Batching
+{
+    [TestFixture]
+    public class ImmediateBatchQueuerTests
+    {
+        [Test]
+        public void ImmediateBatchQueuer_Constructor()
+        {
+            var graphicsDevice = TestHelper.CreateGraphicsDevice();
+            var basicEffect = new BasicEffect(graphicsDevice);
+            var defaultDrawContext = new EffectDrawContext<BasicEffect>(basicEffect);
+            var userPrimitivesBatcher = new UserPrimitivesBatchDrawer<TestVertex>(graphicsDevice, defaultDrawContext);
+            var immediateBatchQueuer = new ImmediateBatchQueuer<TestVertex>(userPrimitivesBatcher);
+            Assert.IsNotNull(immediateBatchQueuer);
+        }
+    }
+}
