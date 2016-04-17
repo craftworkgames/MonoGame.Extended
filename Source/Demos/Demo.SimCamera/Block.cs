71fca54cde098d306241b9ed96190bcfbb33b70c
Maties7 <maties7@gmail.com>
2016-04-17 23:57:56 +0200

    Drawing box.

---
+++ Source/Demos/Demo.SimCamera/Block.cs
@@ -0,0 +1,97 @@
+﻿
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.SimCamera
+{
+    public class Block
+    {
+        private Texture2D _texture;
+        private Body _body;
+
+        private float _blockVelocity = 3;
+
+        public Block(World world, Vector2 position, Texture2D texture)
+        {
+            _body = BodyFactory.CreateRectangle(world, 2f, 1f, 1f);
+            _body.BodyType = BodyType.Dynamic;
+            _body.Position = position;
+
+            _texture = texture;
+        }
+
+        public void Update(GameTime time)
+        {
+            bool anyKeyPressed = false;
+
+            var vectorVelocity = new Vector2();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                vectorVelocity.X -= _blockVelocity;
+                anyKeyPressed = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                vectorVelocity.X += _blockVelocity;
+                anyKeyPressed = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            {
+                vectorVelocity.Y -= _blockVelocity;
+                anyKeyPressed = true;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            {
+                vectorVelocity.Y += _blockVelocity;
+                anyKeyPressed = true;
+            }
+
+            if (anyKeyPressed)
+            {
+                _body.LinearVelocity = vectorVelocity;
+            }
+            else
+            {
+                _body.LinearVelocity = new Vector2(0, 0);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                _texture,
+                ConvertSimUnits.ToDisplayUnits(_body.Position),
+                null,
+                Color.White,
+                _body.Rotation,
+                new Vector2(100, 50),
+                1f,
+                SpriteEffects.None,
+                0f
+            );
+            //animatedSprite.Draw(
+            //    spriteBatch,
+            //    new Vector2(
+            //        ConvertUnits.ToDisplayUnits(Player.Position.X),
+            //        ConvertUnits.ToDisplayUnits(Player.Position.Y)
+            //        ),
+            //    Player.Rotation,
+            //    new Vector2(animatedSprite.TextureWidth / 2, animatedSprite.TextureHeight / 2),
+            //    Color.White
+            //    );
+        }
+    }
+}
--- Source/Demos/Demo.SimCamera/Game1.cs
+++ Source/Demos/Demo.SimCamera/Game1.cs
@@ -1,4 +1,5 @@
-﻿using FarseerPhysics.DebugView;
+﻿using FarseerPhysics.Common;
+using FarseerPhysics.DebugView;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
@@ -14,15 +15,18 @@ namespace Demo.SimCamera
     /// </summary>
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
 
         private World _world;
         private DebugViewXNA _debugView;
 
+        private Block _blockControlled;
+        private Block _block;
+
         public Game1()
         {
-            graphics = new GraphicsDeviceManager(this);
+            _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
@@ -44,16 +48,41 @@ namespace Demo.SimCamera
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture2D textureBlock = Content.Load<Texture2D>("block");
 
             _world = new World(new Vector2(0, 0));
+
+            float simWidth = ConvertSimUnits.ToSimUnits(_graphics.GraphicsDevice.Viewport.Width);
+            float simHeight = ConvertSimUnits.ToSimUnits(_graphics.GraphicsDevice.Viewport.Height);
+
+            Vertices borders = new Vertices(3);
+            borders.Add(new Vector2(0, 0));
+            borders.Add(new Vector2(0, simHeight));
+            borders.Add(new Vector2(simWidth, simHeight));
+            borders.Add(new Vector2(simWidth, 0));
+
+            BodyFactory.CreateLoopShape(_world, borders);
+
             _debugView = new DebugViewXNA(_world);
             _debugView.LoadContent(GraphicsDevice, Content);
-            Body body = BodyFactory.CreateRectangle(_world, 2f, 2f, 1f);
-            body.BodyType = BodyType.Dynamic;
-            body.FixedRotation = true;
-            body.Position = new Vector2(0,0);
-            body.ApplyTorque(100);
+
+            _blockControlled = new Block(
+                _world, 
+                new Vector2(
+                    ConvertSimUnits.ToSimUnits(110), 
+                    ConvertSimUnits.ToSimUnits(110)
+                    ),
+                textureBlock);
+
+            _block = new Block(
+                _world, 
+                new Vector2(
+                    ConvertSimUnits.ToSimUnits(410), 
+                    ConvertSimUnits.ToSimUnits(110)
+                    ), 
+                textureBlock);
+
         }
 
         /// <summary>
@@ -74,6 +103,8 @@ namespace Demo.SimCamera
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            _blockControlled.Update(gameTime);
+
             _world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalSeconds, (1f / 60f)));
             base.Update(gameTime);
         }
@@ -87,11 +118,16 @@ namespace Demo.SimCamera
             GraphicsDevice.Clear(Color.CornflowerBlue);
             Matrix projection = Matrix.CreateOrthographicOffCenter(
                 0f, 
-                ConvertSimUnits.ToSimUnits(graphics.GraphicsDevice.Viewport.Width),
-                ConvertSimUnits.ToSimUnits(graphics.GraphicsDevice.Viewport.Height), 
+                ConvertSimUnits.ToSimUnits(_graphics.GraphicsDevice.Viewport.Width),
+                ConvertSimUnits.ToSimUnits(_graphics.GraphicsDevice.Viewport.Height), 
                 0f, 
                 0f, 
             1f);
+
+            _spriteBatch.Begin();
+            _blockControlled.Draw(_spriteBatch);
+            _block.Draw(_spriteBatch);
+            _spriteBatch.End();
 
             _debugView.RenderDebugData(ref projection);
 
