0771868bdebcb378af63693e6028c3cb9aab0ae8
ExCellRaD <jasper.desutter@gmail.com>
2016-05-03 22:49:05 +0200

    angle + tests

---
+++ Source/MonoGame.Extended.Tests/AngleTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+
+namespace MonoGame.Extended.Tests
+{
+    [TestFixture]
+    public class AngleTest
+    {
+        private const double DELTA = 0.000000000001;
+        [Test]
+        public void ConstructorTest()
+        {
+            var value = 0.5;
+            var radians = new Angle(value);
+            var degrees = new Angle(value, AngleType.Degree);
+            var gradians = new Angle(value, AngleType.Gradian);
+            var revolutions = new Angle(value, AngleType.Revolution);
+            Assert.AreEqual(value, radians.Radians, DELTA);
+            Assert.AreEqual(value, degrees.Degrees, DELTA);
+            Assert.AreEqual(value, gradians.Gradians, DELTA);
+            Assert.AreEqual(value, revolutions.Revolutions, DELTA);
+        }
+
+        [Test]
+        public void ConversionTest()
+        {
+            //from radians
+            var radians = new Angle(Math.PI);
+            Assert.AreEqual(180d, radians.Degrees, DELTA);
+            Assert.AreEqual(200d, radians.Gradians, DELTA);
+            Assert.AreEqual(0.5, radians.Revolutions, DELTA);
+            //to radians
+            var degrees = new Angle(180d, AngleType.Degree);
+            var gradians = new Angle(200d, AngleType.Gradian);
+            var revolutions = new Angle(0.5, AngleType.Revolution);
+            Assert.AreEqual(Math.PI, degrees.Radians, DELTA);
+            Assert.AreEqual(Math.PI, gradians.Radians, DELTA);
+            Assert.AreEqual(Math.PI, revolutions.Radians, DELTA);
+        }
+
+        [Test]
+        public void WrapTest()
+        {
+            for (var d = -10d; d < 10d; d += 0.1d)
+            {
+                var wrappositive = new Angle(d);
+                wrappositive.WrapPositive();
+
+                var wrap = new Angle(d);
+                wrap.Wrap();
+
+                Assert.GreaterOrEqual(wrappositive.Radians, 0);
+                Assert.Less(wrappositive.Radians, 2d * Math.PI);
+
+                Assert.GreaterOrEqual(wrap.Radians, -Math.PI);
+                Assert.Less(wrap.Radians, Math.PI);
+            }
+        }
+
+        [Test]
+        public void VectorTest()
+        {
+            var angle = Angle.FromVector(Vector2.One);
+            Assert.AreEqual(-Math.PI / 4d, angle.Radians, DELTA);
+            Assert.AreEqual(10f, angle.ToVector(10f).Length());
+
+            angle = Angle.FromVector(Vector2.UnitX);
+            Assert.AreEqual(0, angle.Radians, DELTA);
+            Assert.IsTrue(Vector2.UnitX.EqualsWithTolerance(angle.ToUnitVector()));
+
+            angle = Angle.FromVector(-Vector2.UnitY);
+            Assert.AreEqual(Math.PI / 2d, angle.Radians, DELTA);
+            Assert.IsTrue((-Vector2.UnitY).EqualsWithTolerance(angle.ToUnitVector()));
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            var angle1 = new Angle(0);
+            var angle2 = new Angle(Math.PI * 2d);
+            Assert.IsTrue(angle1 == angle2);
+            angle2.Radians = Math.PI * 4d;
+            Assert.IsTrue(angle1.Equals(angle2));
+        }
+
+    }
+}
---
+++ Source/MonoGame.Extended/Angle.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended
+{
+    public enum AngleType : byte
+    {
+        Radian = 0,
+        Degree,
+        Revolution, //or Turn / cycle
+        Gradian // or Gon
+    }
+    [DataContract]
+    [DebuggerDisplay("{ToString(),nq}")]
+    public struct Angle : IComparable<Angle>, IEquatable<Angle>
+    {
+        private const double TAU = Math.PI * 2.0;
+        private const double TAU_INV = 0.5 / Math.PI;
+        private const double DEGREE_RADIAN = Math.PI / 180.0;
+        private const double RADIAN_DEGREE = 180.0 / Math.PI;
+        private const double GRADIAN_RADIAN = Math.PI / 200.0;
+        private const double RADIAN_GRADIAN = 200.0 / Math.PI;
+
+        [DataMember]
+        public double Radians { get; set; }
+
+        public double Degrees
+        {
+            get { return Radians * RADIAN_DEGREE; }
+            set { Radians = value * DEGREE_RADIAN; }
+        }
+
+        public double Gradians
+        {
+            get { return Radians * RADIAN_GRADIAN; }
+            set { Radians = value * GRADIAN_RADIAN; }
+        }
+
+        public double Revolutions
+        {
+            get { return Radians * TAU_INV; }
+            set { Radians = value * TAU; }
+        }
+
+        public Angle(double value, AngleType angleType = AngleType.Radian)
+        {
+            switch (angleType)
+            {
+                default:
+                    Radians = 0d;
+                    break;
+                case AngleType.Radian:
+                    Radians = value;
+                    break;
+                case AngleType.Degree:
+                    Radians = value * DEGREE_RADIAN;
+                    break;
+                case AngleType.Revolution:
+                    Radians = value * TAU;
+                    break;
+                case AngleType.Gradian:
+                    Radians = value * GRADIAN_RADIAN;
+                    break;
+            }
+        }
+
+        public double GetValue(AngleType angleType)
+        {
+            switch (angleType)
+            {
+                default:
+                    return 0;
+                case AngleType.Radian:
+                    return Radians;
+                case AngleType.Degree:
+                    return Degrees;
+                case AngleType.Revolution:
+                    return Revolutions;
+                case AngleType.Gradian:
+                    return Gradians;
+            }
+        }
+
+        public void Wrap()
+        {
+            var angle = Radians % TAU;
+            if (angle <= Math.PI) angle += TAU;
+            if (angle > Math.PI) angle -= TAU;
+            Radians = angle;
+        }
+
+        public void WrapPositive()
+        {
+            Radians %= TAU;
+            if (Radians < 0d) Radians += TAU;
+            Radians = Radians;
+        }
+
+        public static Angle FromVector(Vector2 vector)
+        {
+            return new Angle(Math.Atan2(-vector.Y, vector.X));
+        }
+
+        public Vector2 ToUnitVector() => ToVector(1);
+
+        public Vector2 ToVector(float length)
+        {
+            return new Vector2(length * (float)Math.Cos(Radians), -length * (float)Math.Sin(Radians));
+        }
+
+        public static bool IsBetween(Angle value, Angle min, Angle end)
+        {
+            return end < min ?
+                value >= min || value <= end :
+                value >= min && value <= end;
+        }
+
+        public int CompareTo(Angle other)
+        {
+            WrapPositive();
+            other.WrapPositive();
+            return Radians.CompareTo(other.Radians);
+        }
+
+        public bool Equals(Angle other)
+        {
+            WrapPositive();
+            other.WrapPositive();
+            return Radians == other.Radians;
+        }
+
+        #region operators
+        public static implicit operator double(Angle angle)
+        {
+            return angle.Radians;
+        }
+        public static implicit operator float(Angle angle)
+        {
+            return (float)angle.Radians;
+        }
+        public static explicit operator Angle(double angle)
+        {
+            return new Angle(angle);
+        }
+        public static explicit operator Angle(float angle)
+        {
+            return new Angle(angle);
+        }
+        public static Angle operator -(Angle angle)
+        {
+            return new Angle(-angle.Radians);
+        }
+        public static bool operator ==(Angle a, Angle b)
+        {
+            return a.Equals(a);
+        }
+
+        public static bool operator !=(Angle a, Angle b)
+        {
+            return !a.Equals(a);
+        }
+
+        public static Angle operator -(Angle left, Angle right)
+        {
+            return new Angle(left.Radians - right.Radians);
+        }
+        public static Angle operator *(Angle left, double right)
+        {
+            return new Angle(left.Radians * right);
+        }
+        public static Angle operator *(double left, Angle right)
+        {
+            return new Angle(right.Radians * left);
+        }
+        public static Angle operator +(Angle left, Angle right)
+        {
+            return new Angle(left.Radians + right.Radians);
+        }
+        #endregion
+
+
+        public override string ToString()
+        {
+            return $"{Radians} Radians";
+        }
+    }
+}
