03a809a67dd954a2cc3fecc2149a6d2718e67254
Dylan Wilson <dylan@craftworkgames.com>
2016-06-20 21:16:31 +1000

    started roughing out loading gui files

---
+++ Source/MonoGame.Extended/Gui/MonoGameColorJsonConverter.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Globalization;
+using Microsoft.Xna.Framework;
+using Newtonsoft.Json;
+
+namespace MonoGame.Extended.Gui
+{
+    public class MonoGameColorJsonConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var color = (Color)value;
+            writer.WriteValue($"#{color.R:x}{color.G:x}{color.B:x}{color.A:x}");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Color);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var value = (string)reader.Value;
+            var r = int.Parse(value.Substring(1, 2), NumberStyles.HexNumber);
+            var g = int.Parse(value.Substring(3, 2), NumberStyles.HexNumber);
+            var b = int.Parse(value.Substring(5, 2), NumberStyles.HexNumber);
+            var a = int.Parse(value.Substring(7, 2), NumberStyles.HexNumber);
+            return new Color(r, g, b, a);
+        }
+    }
+}
