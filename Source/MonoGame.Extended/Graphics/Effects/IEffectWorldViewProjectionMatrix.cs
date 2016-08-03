e8e21035329d34209568f8676857b47cdf3572e5
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-08-02 18:05:23 -0700

    Add IEffectWorldViewProjectionMatrix

---
+++ Source/MonoGame.Extended/Graphics/Effects/IEffectWorldViewProjectionMatrix.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended.Graphics.Effects
+{
+    public interface IEffectWorldViewProjectionMatrix
+    {
+        Matrix World { get; set; }
+        Matrix View { get; set; }
+        Matrix Projection { get; set; }
+
+        void SetWorld(ref Matrix world);
+        void SetView(ref Matrix view);
+        void SetProjection(ref Matrix projection);
+    }
+}
