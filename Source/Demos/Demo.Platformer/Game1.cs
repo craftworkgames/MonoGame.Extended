13fafbabd6589091a5b3118941b1dc814e8c6bd3
Dylan Wilson <dylan@craftworkgames.com>
2016-08-12 22:17:53 +1000

    started a platformer demo

---
+++ Source/Demos/Demo.Platformer/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Maps.Tiled;
+using MonoGame.Extended.Sprites;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace Demo.Platformer
+{
+    public class Game1 : Game
+    {
+        // ReSharper disable once NotAccessedField.Local
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+        private SpriteBatch _spriteBatch;
+        private Camera2D _camera;
+        private TiledMap _tiledMap;
+
+        public Game1()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480);
+            _camera = new Camera2D(viewportAdapter);
+
+            _tiledMap = Content.Load<TiledMap>("level-1");
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _tiledMap.Draw(_camera);
+
+            base.Draw(gameTime);
+        }
+    }
+}
