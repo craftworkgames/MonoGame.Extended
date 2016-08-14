68d801f52ef13bd7d5af6966498ec173e025e971
Dylan Wilson <dylan@craftworkgames.com>
2016-08-14 20:30:22 +1000

    slowly throwing together a little character

--- Source/Demos/Demo.Platformer/Game1.cs
+++ Source/Demos/Demo.Platformer/Game1.cs
@@ -2,8 +2,10 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
+using MonoGame.Extended.Animations.SpriteSheets;
 using MonoGame.Extended.Maps.Tiled;
 using MonoGame.Extended.Sprites;
+using MonoGame.Extended.TextureAtlases;
 using MonoGame.Extended.ViewportAdapters;
 
 namespace Demo.Platformer
@@ -15,6 +17,8 @@ namespace Demo.Platformer
         private SpriteBatch _spriteBatch;
         private Camera2D _camera;
         private TiledMap _tiledMap;
+        private Sprite _sprite;
+        private SpriteSheetAnimator _animator;
 
         public Game1()
         {
@@ -31,6 +35,13 @@ namespace Demo.Platformer
             var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480);
             _camera = new Camera2D(viewportAdapter);
 
+            var texture = Content.Load<Texture2D>("tiny-characters");
+            var atlas = TextureAtlas.Create(texture, 32, 32, 15);
+            var animationFactory = new SpriteSheetAnimationFactory(atlas);
+            animationFactory.Add("idle", new SpriteSheetAnimationData(new[] {0, 1, 2, 3}, isReversed: true));
+            _animator = new SpriteSheetAnimator(animationFactory);
+            _animator.Play("idle");
+            _sprite = _animator.CreateSprite(position: new Vector2(116, 273));
             _tiledMap = Content.Load<TiledMap>("level-1");
         }
 
@@ -46,6 +57,14 @@ namespace Demo.Platformer
             if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+                _sprite.Position += new Vector2(150, 0) * deltaTime;
+
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+                _sprite.Position -= new Vector2(150, 0) * deltaTime;
+
+            _animator.Update(deltaTime);
+
             base.Update(gameTime);
         }
 
@@ -54,6 +73,10 @@ namespace Demo.Platformer
             GraphicsDevice.Clear(Color.Black);
 
             _tiledMap.Draw(_camera);
+
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: _camera.GetViewMatrix());
+            _spriteBatch.Draw(_sprite);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
