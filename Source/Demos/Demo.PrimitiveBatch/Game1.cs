2b780d011b33b7a23b0b87b92b29121ad94474ef
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-08-23 20:51:24 -0700

    update demo

--- Source/Demos/Demo.PrimitiveBatch/Game1.cs
+++ Source/Demos/Demo.PrimitiveBatch/Game1.cs
@@ -7,6 +7,13 @@ using MonoGame.Extended.TextureAtlases;
 
 namespace Demo.Batching
 {
+    public struct SpriteInfo
+    {
+        public Vector2 Position;
+        public float Rotation;
+        public Color Color;
+    }
+
     public class Game1 : Game
     {
         // ReSharper disable once NotAccessedField.Local
@@ -24,14 +31,15 @@ namespace Demo.Batching
 //        private short[] _polygonIndices;
 //        // the curve (continous line segements)
 //        private VertexPositionColor[] _curveVertices;
-
-        // the rotation angle of the sprite
-        private float _spriteRotation;
 //
 //        // primitives matrix transformation chain
 //        private Matrix _primitivesModelToWorld;
 //        private Matrix _primitivesWorldToView;
 //        private Matrix _primitivesViewToProjetion;
+
+        private readonly FastRandom _random = new FastRandom();
+
+        private readonly SpriteInfo[] _sprites = new SpriteInfo[2048];
 
         public Game1()
         {
@@ -39,6 +47,9 @@ namespace Demo.Batching
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
+
+            _graphicsDeviceManager.PreferredBackBufferWidth = 1920;
+            _graphicsDeviceManager.PreferredBackBufferHeight = 1080;
         }
 
         protected override void LoadContent()
@@ -65,40 +76,52 @@ namespace Demo.Batching
             // load the texture for the sprites
             _spriteTexture = Content.Load<Texture2D>(assetName: "logo-square-128");
             _spriteTextureRegion = new TextureRegion2D(_spriteTexture);
-           
-//            // create our polygon mesh; vertices are in Local space; indices are index references to the vertices to draw 
-//            // indices have to multiple of 3 for PrimitiveType.TriangleList which says to draw a collection of triangles each with 3 vertices (different triangles can share vertices) 
-//            // here we have 2 triangles in the list to form a quad or rectangle: http://wiki.lwjgl.org/images/a/a8/QuadVertices.png
-//            // TriangleList is the most common scenario to have polygon vertices layed out in memory for uploading to the GPU
-//            _polygonVertices = new[]
-//            {
-//                new VertexPositionColor(position: new Vector3(x: 0, y: 0, z: 0), color: Color.Red),
-//                new VertexPositionColor(position: new Vector3(x: 2, y: 0, z: 0), color: Color.Blue),
-//                new VertexPositionColor(position: new Vector3(x: 1, y: 2, z: 0), color: Color.Green),
-//                new VertexPositionColor(position: new Vector3(x: 3, y: 2, z: 0), color: Color.White)
-//            };
-//            _polygonIndices = new short[]
-//            {
-//                1,
-//                0,
-//                2,
-//                1,
-//                2,
-//                3
-//            };
-//
-//            // create our curve as an approximation by a series of line segments; vertices are in Local space; no indices
-//            // LineStrip joins the vertices given in order into a continuous series of line segments
-//            var curveVertices = new List<VertexPositionColor>();
-//            var angleStep = MathHelper.ToRadians(degrees: 1);
-//            const int circlesCount = 3;
-//            for (var angle = 0.0f; angle <= MathHelper.TwoPi * circlesCount; angle += angleStep)
-//            {
-//                var vertexPosition = new Vector3(x: (float)Math.Sin(angle) - angle / 10, y: (float)Math.Cos(angle) - angle / 15, z: 0);
-//                var vertex = new VertexPositionColor(vertexPosition, Color.White);
-//                curveVertices.Add(vertex);
-//            }
-//            _curveVertices = curveVertices.ToArray();
+
+            //            // create our polygon mesh; vertices are in Local space; indices are index references to the vertices to draw 
+            //            // indices have to multiple of 3 for PrimitiveType.TriangleList which says to draw a collection of triangles each with 3 vertices (different triangles can share vertices) 
+            //            // here we have 2 triangles in the list to form a quad or rectangle: http://wiki.lwjgl.org/images/a/a8/QuadVertices.png
+            //            // TriangleList is the most common scenario to have polygon vertices layed out in memory for uploading to the GPU
+            //            _polygonVertices = new[]
+            //            {
+            //                new VertexPositionColor(position: new Vector3(x: 0, y: 0, z: 0), color: Color.Red),
+            //                new VertexPositionColor(position: new Vector3(x: 2, y: 0, z: 0), color: Color.Blue),
+            //                new VertexPositionColor(position: new Vector3(x: 1, y: 2, z: 0), color: Color.Green),
+            //                new VertexPositionColor(position: new Vector3(x: 3, y: 2, z: 0), color: Color.White)
+            //            };
+            //            _polygonIndices = new short[]
+            //            {
+            //                1,
+            //                0,
+            //                2,
+            //                1,
+            //                2,
+            //                3
+            //            };
+            //
+            //            // create our curve as an approximation by a series of line segments; vertices are in Local space; no indices
+            //            // LineStrip joins the vertices given in order into a continuous series of line segments
+            //            var curveVertices = new List<VertexPositionColor>();
+            //            var angleStep = MathHelper.ToRadians(degrees: 1);
+            //            const int circlesCount = 3;
+            //            for (var angle = 0.0f; angle <= MathHelper.TwoPi * circlesCount; angle += angleStep)
+            //            {
+            //                var vertexPosition = new Vector3(x: (float)Math.Sin(angle) - angle / 10, y: (float)Math.Cos(angle) - angle / 15, z: 0);
+            //                var vertex = new VertexPositionColor(vertexPosition, Color.White);
+            //                curveVertices.Add(vertex);
+            //            }
+            //            _curveVertices = curveVertices.ToArray();
+
+            var viewport = GraphicsDevice.Viewport;
+
+            // ReSharper disable once ForCanBeConvertedToForeach
+            for (var index = 0; index < _sprites.Length; index++)
+            {
+                var sprite = _sprites[index];
+                sprite.Position = new Vector2(x: _random.Next(viewport.X, viewport.Width), y: _random.Next(viewport.Y, viewport.Height));
+                sprite.Rotation = MathHelper.ToRadians(degrees: _random.Next(min: 0, max: 360));
+                sprite.Color = Color.FromNonPremultiplied(r: _random.Next(min: 0, max: 255), g: _random.Next(min: 0, max: 255), b: _random.Next(min: 0, max: 255), a: 255);
+                _sprites[index] = sprite;
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -119,7 +142,7 @@ namespace Demo.Batching
             // however, it's left here indicating it's possible and common to change the state between frames
             // use alphablend so the transparent part of the texture is blended with the color behind it
             GraphicsDevice.BlendState = BlendState.AlphaBlend;
-            GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+            GraphicsDevice.SamplerStates[index: 0] = SamplerState.PointClamp;
 
             // draw the polygon and curve in the cartesian coordinate system using the VertexPositionColor PrimitiveBatch
             //            _primitiveBatchPositionColor.Begin();
@@ -130,10 +153,17 @@ namespace Demo.Batching
             // draw the sprite
             _batch.Begin();
             var spriteOrigin = new Vector2(x: _spriteTextureRegion.Texture.Width * 0.5f, y: _spriteTextureRegion.Texture.Height * 0.5f);
-            var spritePosition = new Vector2(x: 150, y: 150);
-            _spriteRotation += MathHelper.ToRadians(degrees: 1);
-            var matrix = Matrix2D.CreateScale(Vector2.One) * Matrix2D.CreateRotationZ(-_spriteRotation) * Matrix2D.CreateTranslation(spritePosition);
-            _batch.DrawSprite(_spriteTextureRegion, ref matrix, origin: spriteOrigin);
+
+            for (var index = 0; index < _sprites.Length; index++)
+            {
+                var sprite = _sprites[index];
+                sprite.Rotation += MathHelper.ToRadians(degrees: 1);
+                _sprites[index] = sprite;
+
+                var matrix = Matrix2D.CreateScale(Vector2.One) * Matrix2D.CreateRotationZ(-sprite.Rotation) * Matrix2D.CreateTranslation(sprite.Position);
+                _batch.DrawSprite(_spriteTextureRegion, ref matrix, origin: spriteOrigin, color: sprite.Color);
+            }
+
             _batch.End();
 
             base.Draw(gameTime);
