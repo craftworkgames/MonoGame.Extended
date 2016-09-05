881df72ce2cf739a88d71e94b976a4dd72b6c6f6
Lucas Girouard-Stranks <lustranks@gmail.com>
2016-09-05 10:44:43 -0700

    explicit polygon

---
+++ Source/MonoGame.Extended/Shapes/Explicit/ExplicitConvexPolygon.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended.Shapes.Explicit
+{
+    public class ExplicitConvexPolygon : ExplicitPolygon
+    {
+        public ExplicitConvexPolygon(Vector2[] vertices)
+            : base(vertices)
+        {
+        }
+
+        protected override void CalculateCentroid(out Vector2 centroid)
+        {
+            centroid.X = 0;
+            centroid.Y = 0;
+            var vertices = Vertices;
+            var verticesCount = vertices.Count;
+
+            for (var i = 0; i < verticesCount; i++)
+            {
+                centroid.X += vertices[i].X;
+                centroid.Y += vertices[i].Y;
+            }
+
+            centroid /= verticesCount;
+        }
+
+    }
+}
---
+++ Source/MonoGame.Extended/Shapes/Explicit/ExplicitPolygon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended.Shapes.Explicit
+{
+    public abstract class ExplicitPolygon
+    {
+        private Vector2[] _vertices;
+
+        public IReadOnlyList<Vector2> Vertices => _vertices;
+
+        public Vector2 Centroid { get; private set; }
+
+        public event Action ShapeChanged;
+
+        protected ExplicitPolygon(Vector2[] vertices)
+        {
+            SetVertices(vertices);
+        }
+
+        public void SetVertices(Vector2[] vertices)
+        {
+            _vertices = vertices;
+            OnShapeChanged();
+            ShapeChanged?.Invoke();
+        }
+
+        protected virtual void OnShapeChanged()
+        {
+            Vector2 centroid;
+            CalculateCentroid(out centroid);
+            Centroid = centroid;
+        }
+
+        protected abstract void CalculateCentroid(out Vector2 centroid);
+    }
+}
