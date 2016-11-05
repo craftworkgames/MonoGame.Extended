620006b4593c6d40822a557b0753d2f211eacb60
Matthew Tornetta <Matthew.Tornetta@gmail.com>
2016-11-05 10:28:07 -0400

    Only override on mono (linux)... not sure why this is needed, will look into later

--- Source/MonoGame.Extended.Tests/MockGameWindow.cs
+++ Source/MonoGame.Extended.Tests/MockGameWindow.cs
@@ -32,9 +32,12 @@ namespace MonoGame.Extended.Tests
         public override bool AllowUserResizing { get; set; }
         public override Rectangle ClientBounds { get; }
         public override Point Position { get; set; }
-        public override Icon Icon { get; set; }
         public override DisplayOrientation CurrentOrientation { get; }
         public override IntPtr Handle { get; }
         public override string ScreenDeviceName { get; }
+
+#if __MonoCS__
+        public override Icon Icon { get; set; }
+#endif
     }
 }
