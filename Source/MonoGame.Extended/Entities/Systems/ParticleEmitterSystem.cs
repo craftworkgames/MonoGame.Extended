76b18691bfd18cb8eb89dfd82d52fa0ea8256beb
Robert Hernandez <killerjaguar26@gmail.com>
2017-01-20 15:54:18 -0500

    Commented out ParticleEmitterSystem as it needs changing
    ParticleEmitter and ParticleEmitterSystem need modifying to the new ECS system. I'm unsure how to decouple them, so I'm not touching it for the time being.

--- Source/MonoGame.Extended/Entities/Systems/ParticleEmitterSystem.cs
+++ Source/MonoGame.Extended/Entities/Systems/ParticleEmitterSystem.cs
@@ -7,11 +7,13 @@ namespace MonoGame.Extended.Entities.Systems
     {
         public override void Update(GameTime gameTime)
         {
-            var deltaTime = gameTime.GetElapsedSeconds();
-            var emitters = GetComponents<ParticleEmitter>();
+            // TODO Reimplement this class to fit into the new ECS system
 
-            foreach (var particleEmitter in emitters)
-                particleEmitter.Update(deltaTime);
+            //var deltaTime = gameTime.GetElapsedSeconds();
+            //var emitters = GetComponents<ParticleEmitter>();
+
+            //foreach (var particleEmitter in emitters)
+            //    particleEmitter.Update(deltaTime);
         }
     }
 }
