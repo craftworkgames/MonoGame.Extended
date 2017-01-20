de8cac5bd6d927940d9190c6811052b2be2c6c58
Robert Hernandez <killerjaguar26@gmail.com>
2017-01-20 15:43:56 -0500

    Created a GenericCollectionComponent

---
+++ Source/MonoGame.Extended/Entities/Components/GenericCollectionComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MonoGame.Extended.Entities.Components
+{
+    public class GenericCollectionComponent<T>
+        : CollectionComponent<T, CollectionComponentEventArgs<T>>
+    {
+        new public IReadOnlyCollection<T> Collection => base.Collection;
+
+        protected override CollectionComponentEventArgs<T> CreateEventArgs(T item)
+        {
+            return new CollectionComponentEventArgs<T>(Entity, item);
+        }
+    }
+}
