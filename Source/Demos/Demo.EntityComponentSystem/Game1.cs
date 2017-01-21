fb2986d981e832f64afb47ea0f182e0c6b106d0b
Robert Hernandez <killerjaguar26@gmail.com>
2017-01-21 18:23:06 -0500

    Updated EntityComponentSystem demo to use new system

--- Source/Demos/Demo.EntityComponentSystem/Game1.cs
+++ Source/Demos/Demo.EntityComponentSystem/Game1.cs
@@ -5,6 +5,7 @@ using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
 using MonoGame.Extended.Animations.SpriteSheets;
 using MonoGame.Extended.Entities;
+using MonoGame.Extended.Entities.Components;
 using MonoGame.Extended.Entities.Systems;
 using MonoGame.Extended.Particles;
 using MonoGame.Extended.Particles.Modifiers;
@@ -14,98 +15,126 @@ using MonoGame.Extended.Particles.Profiles;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.TextureAtlases;
 using MonoGame.Extended.ViewportAdapters;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework.Content;
 
 namespace Demo.EntityComponentSystem
 {
+    class LogoTag
+    {
+        
+    }
+
+    class LogoSystem : EntitySystem
+    {
+        private Texture2D _texture;
+
+        public override void LoadContent(ContentManager contentManager)
+        {
+            _texture = contentManager.Load<Texture2D>("logo-square-128");
+        }
+
+        public override void EntityCreated(Entity entity)
+        {
+            if (entity.HasComponent<LogoTag>())
+            {
+                SpriteComponent sprite = entity.GetComponent<SpriteComponent>();
+                sprite.TextureRegion = new Sprite(_texture).TextureRegion;
+                sprite.Position = new Vector2(400, 240);
+                sprite.Origin = (sprite.TextureRegion.Size / 2);
+            }
+        }
+
+        public override void Update(Entity entity, GameTime gameTime)
+        {
+            if (entity.HasComponent<LogoTag>())
+                entity.GetComponent<TransformComponent>().Rotation += gameTime.GetElapsedSeconds();
+        }
+    }
+
+    class MotwTag
+    {
+
+    }
+
+    class MotwSystem : EntitySystem
+    {
+        private SpriteSheetAnimationFactory _animation;
+
+        public override void LoadContent(ContentManager contentManager)
+        {
+            var texture = contentManager.Load<Texture2D>("motw");
+            var textureAtlas = TextureAtlas.Create(texture, 52, 72);
+
+            _animation = new SpriteSheetAnimationFactory(textureAtlas);
+            _animation.Add("idle", new SpriteSheetAnimationData(new[] { 0 }));
+            _animation.Add("walkSouth", new SpriteSheetAnimationData(new[] { 0, 1, 2, 1 }, isLooping: true));
+            _animation.Add("walkWest", new SpriteSheetAnimationData(new[] { 12, 13, 14, 13 }, isLooping: true));
+            _animation.Add("walkEast", new SpriteSheetAnimationData(new[] { 24, 25, 26, 25 }, isLooping: true));
+            _animation.Add("walkNorth", new SpriteSheetAnimationData(new[] { 36, 37, 38, 37 }, isLooping: true));
+        }
+
+        public override void EntityCreated(Entity entity)
+        {
+            if (entity.HasComponent<MotwTag>())
+            {
+                var sprite = entity.GetComponent<SpriteComponent>();
+                sprite.AnimationFactory = _animation;
+                sprite.Play("walkSouth");
+                sprite.Position = new Vector2(50, 50);
+                sprite.Origin = sprite.TextureRegion.Size / 2;
+            }
+        }
+    }
+
     public class Game1 : Game
     {
-        // ReSharper disable once NotAccessedField.Local
-        private readonly GraphicsDeviceManager _graphicsDeviceManager;
-        private Camera2D _camera;
-
         private MonoGame.Extended.Entities.EntityComponentSystem _entityComponentSystem;
-        private Entity _entity;
 
         public Game1()
         {
-            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            new GraphicsDeviceManager(this);
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
         }
 
-        protected override void LoadContent()
+        protected override void Initialize()
         {
-            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480);
-            _camera = new Camera2D(viewportAdapter);
+            var camera = new Camera2D(new BoxingViewportAdapter(Window, GraphicsDevice, 800, 480));
+            var spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            _entityComponentSystem = new MonoGame.Extended.Entities.EntityComponentSystem();
-            _entityComponentSystem.RegisterSystem(new SpriteBatchSystem(GraphicsDevice, _camera));
+            _entityComponentSystem = new MonoGame.Extended.Entities.EntityComponentSystem(this);
+            Components.Add(_entityComponentSystem);
+
+            _entityComponentSystem.RegisterComponent<LogoTag>(() => new LogoTag());
+            _entityComponentSystem.RegisterComponent<MotwTag>(() => new MotwTag());
+            _entityComponentSystem.RegisterComponent<TransformComponent>(() => new TransformComponent());
+            _entityComponentSystem.RegisterComponent<SpriteComponent>(() => new SpriteComponent());
+
+            _entityComponentSystem.RegisterEntity("logo", new Type[] { typeof(LogoTag), typeof(SpriteComponent), typeof(TransformComponent) });
+            _entityComponentSystem.RegisterEntity("motw", new Type[] { typeof(MotwTag), typeof(SpriteComponent) });
+
+            _entityComponentSystem.RegisterSystem(new SpriteBatchSystem(spriteBatch, camera));
             _entityComponentSystem.RegisterSystem(new AnimatedSpriteSystem());
             _entityComponentSystem.RegisterSystem(new ParticleEmitterSystem());
+            _entityComponentSystem.RegisterSystem(new LogoSystem());
+            _entityComponentSystem.RegisterSystem(new MotwSystem());
 
-            var logoTexture = Content.Load<Texture2D>("logo-square-128");
-            _entity = _entityComponentSystem.CreateEntity("logo", new Vector2(400, 240));
-            _entity.AttachComponent(new Sprite(logoTexture));
-            
-            var motwTexture = Content.Load<Texture2D>("motw");
-            var motwAtlas = TextureAtlas.Create(motwTexture, 52, 72);
-            var motwAnimationFactory = new SpriteSheetAnimationFactory(motwAtlas);
-            motwAnimationFactory.Add("idle", new SpriteSheetAnimationData(new[] { 0 }));
-            motwAnimationFactory.Add("walkSouth", new SpriteSheetAnimationData(new[] { 0, 1, 2, 1 }, isLooping: false));
-            motwAnimationFactory.Add("walkWest", new SpriteSheetAnimationData(new[] { 12, 13, 14, 13 }, isLooping: false));
-            motwAnimationFactory.Add("walkEast", new SpriteSheetAnimationData(new[] { 24, 25, 26, 25 }, isLooping: false));
-            motwAnimationFactory.Add("walkNorth", new SpriteSheetAnimationData(new[] { 36, 37, 38, 37 }, isLooping: false));
+            _entityComponentSystem.Initialize();
 
-            var animatedEntity = _entityComponentSystem.CreateEntity("animated", new Vector2(50, 50));
-            animatedEntity.AttachComponent(new AnimatedSprite(motwAnimationFactory, "walkSouth"));
+            _entityComponentSystem.CreateEntity("logo");
+            _entityComponentSystem.CreateEntity("motw");
 
-            var particleEntity = _entityComponentSystem.CreateEntity("particles", new Vector2(500, 50));
-            var particleEmitter = new ParticleEmitter(new TextureRegion2D(logoTexture), 500, TimeSpan.FromSeconds(0.5f),
-                Profile.Point())
-            {
-                Parameters = new ParticleReleaseParameters
-                {
-                    Speed = new Range<float>(0f, 150f),
-                    Quantity = 30,
-                    Rotation = new Range<float>(-1f, 1f),
-                    Scale = new Range<float>(3.0f, 4.0f)
-                },
-                Modifiers = new IModifier[]
-                {
-                    new AgeModifier
-                    {
-                        Interpolators = new IInterpolator[]
-                        {
-                            new ColorInterpolator
-                            {
-                                InitialColor = new HslColor(0.8f, 0.8f, 0.8f),
-                                FinalColor = new HslColor(0.5f, 0.9f, 1.0f)
-                            }
-                        }
-                    },
-                    new RotationModifier {RotationRate = -2.1f},
-                    new RectangleContainerModifier {Width = 800, Height = 480},
-                    new LinearGravityModifier {Direction = -Vector2.UnitY, Strength = 130f}
-                }
-            };
-            particleEntity.AttachComponent(particleEmitter);
-        }
-
-        protected override void UnloadContent()
-        {
+            base.Initialize();
         }
 
         protected override void Update(GameTime gameTime)
         {
-            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             var keyboardState = Keyboard.GetState();
-
             if (keyboardState.IsKeyDown(Keys.Escape))
                 Exit();
-
-            _entity.Rotation += deltaTime;
-            _entityComponentSystem.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -113,9 +142,6 @@ namespace Demo.EntityComponentSystem
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-
-            _entityComponentSystem.Draw(gameTime);
-
             base.Draw(gameTime);
         }
     }
