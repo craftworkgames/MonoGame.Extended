76af63a3689d7dcbea2e5823de8088293b5c4206
Robert Hernandez <killerjaguar26@gmail.com>
2017-01-22 01:49:02 -0500

    Added extension methods to register the base components and systems

---
+++ Source/MonoGame.Extended/Entities/EntityComponentSystemExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Entities.Components;
+using MonoGame.Extended.Entities.Systems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGame.Extended.Entities
+{
+    public static class EntityComponentSystemExtensions
+    {
+        public static void RegisterBaseComponents(this EntityComponentSystem ecs)
+        {
+            ecs.RegisterComponent<SpriteComponent>();
+            ecs.RegisterComponent<Transform>();
+        }
+
+        public static void RegisterBaseSystems(this EntityComponentSystem ecs)
+        {
+            ecs.RegisterSystem(new SpriteAnimatorSystem());
+        }
+    }
+}
