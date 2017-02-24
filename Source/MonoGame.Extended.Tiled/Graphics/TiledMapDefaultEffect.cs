a4fe5cafade8bd8a137a480a70582649c374015b
Lucas Girouard-Stranks <lustranks@gmail.com>
2017-02-24 03:51:57 -0800

    TiledMapRenderer allow custom effect (#345)

---
+++ Source/MonoGame.Extended.Tiled/Graphics/TiledMapDefaultEffect.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGame.Extended.Tiled.Graphics
+{
+    public class TiledMapDefaultEffect : BasicEffect, ITiledMapEffect
+    {
+        public TiledMapDefaultEffect(GraphicsDevice device) 
+            : base(device)
+        {
+            TextureEnabled = true;
+        }
+
+        public TiledMapDefaultEffect(BasicEffect cloneSource) 
+            : base(cloneSource)
+        {
+            TextureEnabled = true;
+        }
+    }
+}
