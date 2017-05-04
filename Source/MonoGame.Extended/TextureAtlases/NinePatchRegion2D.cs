1d75323dc7788e6493ef1361e9407b80bffddc4f
Dylan Wilson <dylan@craftworkgames.com>
2017-05-04 22:37:29 +1000

    cache nine patches

--- Source/MonoGame.Extended/TextureAtlases/NinePatchRegion2D.cs
+++ Source/MonoGame.Extended/TextureAtlases/NinePatchRegion2D.cs
@@ -5,7 +5,7 @@ namespace MonoGame.Extended.TextureAtlases
 {
     public class NinePatchRegion2D : TextureRegion2D
     {
-        public Rectangle[] SourcePatches { get; }
+        public Rectangle[] SourcePatches { get; } = new Rectangle[9];
         public Thickness Padding { get; }
         public int LeftPadding => Padding.Left;
         public int TopPadding => Padding.Top;
@@ -16,7 +16,7 @@ namespace MonoGame.Extended.TextureAtlases
             : base(textureRegion.Name, textureRegion.Texture, textureRegion.X, textureRegion.Y, textureRegion.Width, textureRegion.Height)
         {
             Padding = padding;
-            SourcePatches = CreatePatches(textureRegion.Bounds);
+            CachePatches(textureRegion.Bounds, SourcePatches);
         }
 
         public NinePatchRegion2D(TextureRegion2D textureRegion, int padding)
@@ -39,31 +39,46 @@ namespace MonoGame.Extended.TextureAtlases
         {
         }
 
+        public const int TopLeft = 0;
+        public const int TopMiddle = 1;
+        public const int TopRight = 2;
+        public const int MiddleLeft = 3;
+        public const int Middle = 4;
+        public const int MiddleRight = 5;
+        public const int BottomLeft = 6;
+        public const int BottomMiddle = 7;
+        public const int BottomRight = 8;
+
+        private readonly Rectangle[] _destinationPatches = new Rectangle[9];
+
         public Rectangle[] CreatePatches(Rectangle rectangle)
         {
-            var x = rectangle.X;
-            var y = rectangle.Y;
-            var w = rectangle.Width;
-            var h = rectangle.Height;
+            CachePatches(rectangle, _destinationPatches);
+            return _destinationPatches;
+        }
+
+        private void CachePatches(Rectangle sourceRectangle, Rectangle[] patchCache)
+        {
+            var x = sourceRectangle.X;
+            var y = sourceRectangle.Y;
+            var w = sourceRectangle.Width;
+            var h = sourceRectangle.Height;
             var middleWidth = w - LeftPadding - RightPadding;
             var middleHeight = h - TopPadding - BottomPadding;
             var bottomY = y + h - BottomPadding;
             var rightX = x + w - RightPadding;
             var leftX = x + LeftPadding;
             var topY = y + TopPadding;
-            var patches = new[]
-            {
-                new Rectangle(x, y, LeftPadding, TopPadding), // top left
-                new Rectangle(leftX, y, middleWidth, TopPadding), // top middle
-                new Rectangle(rightX, y, RightPadding, TopPadding), // top right
-                new Rectangle(x, topY, LeftPadding, middleHeight), // left middle
-                new Rectangle(leftX, topY, middleWidth, middleHeight), // middle
-                new Rectangle(rightX, topY, RightPadding, middleHeight), // right middle
-                new Rectangle(x, bottomY, LeftPadding, BottomPadding), // bottom left
-                new Rectangle(leftX, bottomY, middleWidth, BottomPadding), // bottom middle
-                new Rectangle(rightX, bottomY, RightPadding, BottomPadding) // bottom right
-            };
-            return patches;
+
+            patchCache[TopLeft] = new Rectangle(x, y, LeftPadding, TopPadding);
+            patchCache[TopMiddle] = new Rectangle(leftX, y, middleWidth, TopPadding);
+            patchCache[TopRight] = new Rectangle(rightX, y, RightPadding, TopPadding);
+            patchCache[MiddleLeft] = new Rectangle(x, topY, LeftPadding, middleHeight);
+            patchCache[Middle] = new Rectangle(leftX, topY, middleWidth, middleHeight);
+            patchCache[MiddleRight] = new Rectangle(rightX, topY, RightPadding, middleHeight);
+            patchCache[BottomLeft] = new Rectangle(x, bottomY, LeftPadding, BottomPadding);
+            patchCache[BottomMiddle] = new Rectangle(leftX, bottomY, middleWidth, BottomPadding);
+            patchCache[BottomRight] = new Rectangle(rightX, bottomY, RightPadding, BottomPadding);
         }
     }
 }
