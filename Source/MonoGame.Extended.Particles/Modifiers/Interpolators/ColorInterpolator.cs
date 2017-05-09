ad1f656f4bfe5c8de89bf9a00b3fd015bd37a7c6
Dylan Wilson <dylan@craftworkgames.com>
2017-05-09 23:41:15 +1000

    resolved an inconsistency with the interpolators

--- Source/MonoGame.Extended.Particles/Modifiers/Interpolators/ColorInterpolator.cs
+++ Source/MonoGame.Extended.Particles/Modifiers/Interpolators/ColorInterpolator.cs
@@ -1,23 +1,40 @@
-﻿namespace MonoGame.Extended.Particles.Modifiers.Interpolators
+﻿using System;
+
+namespace MonoGame.Extended.Particles.Modifiers.Interpolators
 {
     /// <summary>
     ///     Defines a modifier which interpolates the color of a particle over the course of its lifetime.
     /// </summary>
-    public sealed class ColorInterpolator : IInterpolator
+    public sealed class ColorInterpolator : IInterpolator<HslColor>
     {
         /// <summary>
         ///     Gets or sets the initial color of particles when they are released.
         /// </summary>
-        public HslColor InitialColor { get; set; }
+        public HslColor StartValue { get; set; }
 
         /// <summary>
         ///     Gets or sets the final color of particles when they are retired.
         /// </summary>
-        public HslColor FinalColor { get; set; }
+        public HslColor EndValue { get; set; }
+
+        [Obsolete("Use StartValue instead")]
+        public HslColor InitialColor
+        {
+            get { return StartValue; }
+            set { StartValue = value; }
+        }
+
+        [Obsolete("Use EndValue instead")]
+        public HslColor FinalColor
+        {
+            get { return EndValue; }
+            set { EndValue = value; }
+        }
 
         public unsafe void Update(float amount, Particle* particle)
         {
-            particle->Color = HslColor.Lerp(InitialColor, FinalColor, amount);
+            particle->Color = HslColor.Lerp(StartValue, EndValue, amount);
         }
+
     }
 }
