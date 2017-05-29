df09919af85f44d5523ee6c893499a6111ff74f7
Dylan Wilson <dylan@craftworkgames.com>
2017-05-29 21:43:56 +1000

    changed the particle modifer interface to an abstract base with an optional name

---
+++ Source/MonoGame.Extended.Particles/Modifiers/Modifier.cs
@@ -0,0 +1,13 @@
+﻿namespace MonoGame.Extended.Particles.Modifiers
+{
+    public abstract class Modifier
+    {
+        public string Name { get; set; }
+        public abstract void Update(float elapsedSeconds, ParticleBuffer.ParticleIterator iterator);
+
+        public override string ToString()
+        {
+            return $"{Name} [{GetType().Name}]";
+        }
+    }
+}
