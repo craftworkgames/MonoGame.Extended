ce98a06eebaf30e5ad3c0f5ee84f7e5a65690392
Dylan Wilson <dylan@craftworkgames.com>
2017-06-14 23:07:46 +1000

    custom dependency resolution in the ecs

---
+++ Source/MonoGame.Extended.Entities/DependencyResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonoGame.Extended.Entities
+{
+    public abstract class DependencyResolver
+    {
+        public abstract object Resolve(Type type, params object[] args);
+
+        public T Resolve<T>(Type type, params object[] args)
+        {
+            return (T)Resolve(type, args);
+        }
+    }
+}
