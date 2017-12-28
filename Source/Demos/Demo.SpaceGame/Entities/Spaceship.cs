104667b7e2117c4e96f71bc5147ecb6ec38267ef
Jon Seaman <jonmseaman@live.com>
2017-12-28 05:22:55 -0500

    Fix template manager (#446)
    * Fix Demo.SpaceGame aiming and spaceship control
    
    * Added ECS to template

--- Source/Demos/Demo.SpaceGame/Entities/Spaceship.cs
+++ Source/Demos/Demo.SpaceGame/Entities/Spaceship.cs
@@ -16,7 +16,7 @@ namespace Demo.SpaceGame.Entities
         private float _fireCooldown;
         public CircleF BoundingCircle;
 
-        public Vector2 Direction => Vector2.UnitX.Rotate(Rotation);
+        public Vector2 Direction => (-Vector2.UnitX).Rotate(-Rotation);
 
         public Vector2 Position
         {
@@ -72,7 +72,7 @@ namespace Demo.SpaceGame.Entities
 
         public void LookAt(Vector2 point)
         {
-            Rotation = (float)Math.Atan2(point.Y - Position.Y, point.X - Position.X);
+            Rotation = (float)Math.Atan2(point.Y - Position.Y, Position.X - point.X);
         }
 
         public void Fire()
@@ -83,8 +83,8 @@ namespace Demo.SpaceGame.Entities
             }
 
             var angle = Rotation + MathHelper.ToRadians(90);
-            var muzzle1Position = Position + new Vector2(14, 0).Rotate(angle);
-            var muzzle2Position = Position + new Vector2(-14, 0).Rotate(angle);
+            var muzzle1Position = Position + new Vector2(14, 0).Rotate(-angle);
+            var muzzle2Position = Position + new Vector2(-14, 0).Rotate(-angle);
             _bulletFactory.Create(muzzle1Position, Direction, angle, 550f);
             _bulletFactory.Create(muzzle2Position, Direction, angle, 550f);
             _fireCooldown = 0.2f;
