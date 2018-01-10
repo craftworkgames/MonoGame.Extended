3632123cf6f8febaafdf55b7ca9bcae2988e9f40
Jon Seaman <jonmseaman@live.com>
2018-01-09 22:14:11 -0500

    Added ability to style labels

--- Source/MonoGame.Extended.NuclexGui/Controls/GuiLabelControl.cs
+++ Source/MonoGame.Extended.NuclexGui/Controls/GuiLabelControl.cs
@@ -6,6 +6,19 @@
         /// <summary>Text to be rendered in the control's frame</summary>
         public string Text;
 
+        /// <summary>
+        /// Gets or sets the style to be used with this label when drawing.
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         If you are using the FlatGuiVisualizer, this style
+        ///         corresponds to the 'Frames' used in 
+        ///         <see cref="MonoGame.Extended.NuclexGui.Visuals.Flat.FlatGuiGraphics"/>
+        ///         The style can be customized in the skin's json file.
+        ///     </para>
+        /// </remarks>
+        public string Style { get; set; }
+
         /// <summary>Initializes a new label control with an empty string</summary>
         public GuiLabelControl() : this(string.Empty)
         {
---
+++ Source/Tests/MonoGame.Extended.NuclexGui.Tests/GuiManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Input.InputListeners;
+using NUnit.Framework;
+
+namespace MonoGame.Extended.NuclexGui.Tests
+{
+    [TestFixture]
+    public class GuiManagerTests
+    {
+        [Test]
+        public void SampleGuiManagerTest()
+        {
+            var gameServices = new GameServiceContainer();
+            var guiInputService = new GuiInputService(new InputListenerComponent(null));
+            var gui = new GuiManager(gameServices, guiInputService);
+
+            gui.Update(new GameTime());
+        }
+    }
+}
