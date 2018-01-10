eb5fd93e9f83e7e963aa1d067ceb46c93d8ab581
Jon Seaman <jonmseaman@live.com>
2018-01-09 22:36:20 -0500

    Feature label styling (#454)
    * Added some basic tests for the ECS
    
    * Add test project for Nuclex Gui
    
    * Nuclex Gui project update
    
    * Added ability to style labels
    
    * Removed Nuclex Gui Test project for lack of tests

---
+++ Source/Tests/MonoGame.Extended.Entities.Tests/Implementation/EntityComponentBasic.cs
@@ -0,0 +1,10 @@
+﻿using MonoGame.Extended.Entities;
+
+namespace MonoGame.Extended.Gui.Tests.Implementation
+{
+    [EntityComponent]
+    public class EntityComponentBasic : TransformComponent2D
+    {
+        public int Number { get; set; }
+    }
+}
---
+++ Source/Tests/MonoGame.Extended.Entities.Tests/Implementation/EntityTemplateUsingManager.cs
@@ -0,0 +1,16 @@
+﻿using MonoGame.Extended.Entities;
+
+namespace MonoGame.Extended.Gui.Tests.Implementation
+{
+    [EntityTemplate(Name)]
+    public class EntityTemplateUsingManager : EntityTemplate
+    {
+        public const string Name = nameof(EntityTemplateUsingManager);
+
+        protected override void Build(Entity entity)
+        {
+            var num = Manager.EntityManager.TotalEntitiesCount;
+            entity.Attach<EntityComponentBasic>(c => c.Number = num);
+        }
+    }
+}
