22d04382e93c1e10aa585b3b64c47b0f310403f3
Jon Seaman <jonmseaman@live.com>
2018-03-10 13:17:11 -0500

    Started on tests

---
+++ Source/Tests/MonoGame.Extended.Collisions.Tests/Implementation/BasicActor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended.Collisions;
+
+namespace MonoGame.Extended.Gui.Tests
+{
+    public class BasicActor : IActorTarget
+    {
+        public Vector2 Position { get; set; }
+        public RectangleF BoundingBox { get; set; }
+        public Vector2 Velocity { get; set; }
+        public void OnCollision(CollisionInfo collisionInfo)
+        {
+            if (collisionInfo.Other is BasicActor)
+            {
+                CollisionCount++;
+            }
+        }
+
+        public int CollisionCount { get; set; }
+    }
+}
---
+++ Source/Tests/MonoGame.Extended.Collisions.Tests/UsageTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using MonoGame.Extended.Collisions;
+using NUnit.Framework;
+
+namespace MonoGame.Extended.Gui.Tests
+{
+    /// <summary>
+    /// Simple usage tests showing how to use this library.
+    /// </summary>
+    [TestFixture]
+    public class UsageTests
+    {
+        [Test]
+        public void BasicUsageTest()
+        {
+            var world = new CollisionWorld(Vector2.Zero);
+            var boundingBox = new RectangleF(2, 2, 10, 10);
+
+            var actor1 = new BasicActor {BoundingBox = boundingBox};
+            var actor2 = new BasicActor {BoundingBox = boundingBox};
+
+            world.CreateActor(actor1);
+            world.CreateActor(actor2);
+
+            var data = new int[2] {1, 1};
+            world.CreateGrid(data, 2, 1, 10, 10);
+
+            world.Update(new GameTime(TimeSpan.FromSeconds(0.2), TimeSpan.FromSeconds(0.2)));
+            
+
+            // The two actors should have collided.
+            Assert.AreEqual(actor1.CollisionCount, actor2.CollisionCount);
+            Assert.AreEqual(1, actor1.CollisionCount);
+        }
+    }
+}
