4e02189b96ae6cf51e56adf58e62bc871b377665
Jon Seaman <jonmseaman@live.com>
2018-06-08 11:00:39 -0400

    Docs

--- Source/MonoGame.Extended/ShapeF.cs
+++ Source/MonoGame.Extended/ShapeF.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace MonoGame.Extended
+﻿namespace MonoGame.Extended
 {
     /// <summary>
     ///     Base class for shapes.
@@ -12,8 +10,17 @@ namespace MonoGame.Extended
     {
     }
 
+    /// <summary>
+    ///     Class that implements methods for shared <see cref="IShapeF" /> methods.
+    /// </summary>
     public static class Shape
     {
+        /// <summary>
+        ///     Check if two shapes intersect.
+        /// </summary>
+        /// <param name="a">The first shape.</param>
+        /// <param name="b">The second shape.</param>
+        /// <returns>True if the two shapes intersect.</returns>
         public static bool Intersects(this IShapeF a, IShapeF b)
         {
             var intersects = false;
@@ -21,13 +28,16 @@ namespace MonoGame.Extended
             if (a is RectangleF rectA && b is RectangleF rectB)
             {
                 intersects = rectA.Intersects(rectB);
-            } else if (a is CircleF circA && b is CircleF circB)
+            }
+            else if (a is CircleF circA && b is CircleF circB)
             {
                 intersects = circA.Intersects(circB);
-            } else if (a is RectangleF rect1 && b is CircleF circ1)
+            }
+            else if (a is RectangleF rect1 && b is CircleF circ1)
             {
                 return Intersects(circ1, rect1);
-            } else if (a is CircleF circ2 && b is RectangleF rect2)
+            }
+            else if (a is CircleF circ2 && b is RectangleF rect2)
             {
                 return Intersects(circ2, rect2);
             }
@@ -35,6 +45,12 @@ namespace MonoGame.Extended
             return intersects;
         }
 
+        /// <summary>
+        ///     Checks if a circle and rectangle intersect.
+        /// </summary>
+        /// <param name="circ">Circle to check intersection with rectangle.</param>
+        /// <param name="rect">Rectangle to check intersection with circle.</param>
+        /// <returns>True if the circle and rectangle intersect.</returns>
         public static bool Intersects(CircleF circ, RectangleF rect)
         {
             var boundingRect = new BoundingRectangle(rect.Center, rect.Size / 2);
