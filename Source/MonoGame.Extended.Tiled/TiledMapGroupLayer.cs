6f4e5e2e647d719eb0984c46255b354cf73d7a57
stefanrbk <stefan.kewatt@gmail.com>
2018-07-07 15:18:34 -0500

    Update TiledMapReader to read TiledMapGroupLayers

--- Source/MonoGame.Extended.Tiled/TiledMapGroupLayer.cs
+++ Source/MonoGame.Extended.Tiled/TiledMapGroupLayer.cs
@@ -8,7 +8,7 @@ namespace MonoGame.Extended.Tiled
 	public class TiledMapGroupLayer : TiledMapLayer
 	{
 		public List<TiledMapLayer> Layers { get; }
-		protected TiledMapGroupLayer(string name, List<TiledMapLayer> layers, Vector2? offset = null, float opacity = 1, bool isVisible = true) 
+		public TiledMapGroupLayer(string name, List<TiledMapLayer> layers, Vector2? offset = null, float opacity = 1, bool isVisible = true) 
 			: base(name, offset, opacity, isVisible)
 		{
 			Layers = layers;
