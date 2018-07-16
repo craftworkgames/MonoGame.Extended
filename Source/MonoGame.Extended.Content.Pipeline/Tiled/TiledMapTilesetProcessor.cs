c376c2ff2f14d764821ac47e451d22c490b1dcc9
Raflos10 <34902103+Raflos10@users.noreply.github.com>
2018-07-16 16:48:13 -0700

    Update TiledMapTilesetProcessor.cs
    Forgot to add the properties to this processor too.

--- Source/MonoGame.Extended.Content.Pipeline/Tiled/TiledMapTilesetProcessor.cs
+++ Source/MonoGame.Extended.Content.Pipeline/Tiled/TiledMapTilesetProcessor.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using System;
 using System.Collections.Generic;
@@ -6,32 +6,33 @@ using System.Text;
 
 namespace MonoGame.Extended.Content.Pipeline.Tiled
 {
-	[ContentProcessor(DisplayName = "Tiled Map Tileset Processor - MonoGame.Extended")]
-	public class TiledMapTilesetProcessor : ContentProcessor<TiledMapTilesetContent, TiledMapTilesetContent>
-	{
-		public override TiledMapTilesetContent Process(TiledMapTilesetContent tileset, ContentProcessorContext context)
-		{
-			try
-			{
-				ContentLogger.Logger = context.Logger;
+    [ContentProcessor(DisplayName = "Tiled Map Tileset Processor - MonoGame.Extended")]
+    public class TiledMapTilesetProcessor : ContentProcessor<TiledMapTilesetContent, TiledMapTilesetContent>
+    {
+        public override TiledMapTilesetContent Process(TiledMapTilesetContent tileset, ContentProcessorContext context)
+        {
+            try
+            {
+                ContentLogger.Logger = context.Logger;
 
-				ContentLogger.Log($"Processing tileset '{tileset.Name}'");
-				// Build the Texture2D asset and load it as it will be saved as part of this tileset file.
-				tileset.Image.ContentRef = context.BuildAsset<Texture2DContent, Texture2DContent>(new ExternalReference<Texture2DContent>(tileset.Image.Source), "", new OpaqueDataDictionary() { { "ColorKeyColor", tileset.Image.TransparentColor }, { "ColorKeyEnabled", true } }, "", "");
+                ContentLogger.Log($"Processing tileset '{tileset.Name}'");
+                // Build the Texture2D asset and load it as it will be saved as part of this tileset file.
+                tileset.Image.ContentRef = context.BuildAsset<Texture2DContent, Texture2DContent>(new ExternalReference<Texture2DContent>(tileset.Image.Source), "",
+                    TiledMapProcessor.GetImageParameters(tileset.Properties, tileset.Image), "", "");
 
-				foreach (var tile in tileset.Tiles)
-					foreach (var obj in tile.Objects)
-						TiledMapObjectContent.Process(obj, context);
+                foreach (var tile in tileset.Tiles)
+                    foreach (var obj in tile.Objects)
+                        TiledMapObjectContent.Process(obj, context);
 
-				ContentLogger.Log($"Processed tileset '{tileset.Name}'");
+                ContentLogger.Log($"Processed tileset '{tileset.Name}'");
 
-				return tileset;
-			}
-			catch (Exception ex)
-			{
-				context.Logger.LogImportantMessage(ex.Message);
-				throw ex;
-			}
-		}
-	}
+                return tileset;
+            }
+            catch (Exception ex)
+            {
+                context.Logger.LogImportantMessage(ex.Message);
+                throw ex;
+            }
+        }
+    }
 }
