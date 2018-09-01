bf66afb92619fefec4db55acb76ec20d52b4d7ef
Dylan Wilson <dylan@craftworkgames.com>
2018-09-01 20:52:46 +1000

    experimenting with an XML based GUI markup parser

---
+++ Source/Tools/ContentExplorer/GuiMarkupParser.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Xml;
+using MonoGame.Extended.Gui.Controls;
+
+namespace ContentExplorer
+{
+    public class GuiMarkupParser
+    {
+        public GuiMarkupParser()
+        {
+        }
+
+        private static readonly Dictionary<string, Type> _controlTypes = new[]
+        {
+            typeof(Button),
+            typeof(StackPanel),
+            typeof(DockPanel),
+            typeof(ToggleButton)
+        }.ToDictionary(t => t.Name, StringComparer.OrdinalIgnoreCase);
+
+        private static object ParseChildNode(XmlNode node)
+        {
+            if (node is XmlText)
+                return node.InnerText.Trim();
+
+            if (_controlTypes.TryGetValue(node.Name, out var type))
+            {
+                var typeInfo = type.GetTypeInfo();
+                var item = Activator.CreateInstance(type);
+
+                foreach (var attribute in node.Attributes.Cast<XmlAttribute>())
+                {
+                    var property = typeInfo.GetProperty(attribute.Name);
+
+                    if (property != null)
+                    {
+                        // TODO: handle all the different property types
+                        if (property.PropertyType == typeof(bool))
+                            property.SetValue(item, bool.Parse(attribute.Value));
+                        else
+                            property.SetValue(item, attribute.Value);
+                    }
+                }
+
+
+                if (node.HasChildNodes)
+                {
+                    switch (item)
+                    {
+                        case ContentControl contentControl:
+                            // TOOD: Throw if there's more than one child
+                            contentControl.Content = ParseChildNode(node.ChildNodes[0]);
+                            break;
+                        case LayoutControl layoutControl:
+                            foreach (var control in ParseChildNodes(node.ChildNodes))
+                                layoutControl.Items.Add(control as Control);
+                            break;
+                    }
+                }
+
+                return item;
+            }
+
+            throw new InvalidOperationException($"Unknown control type {node.Name}");
+        }
+
+        private static IEnumerable<object> ParseChildNodes(XmlNodeList nodes)
+        {
+            foreach (var node in nodes.Cast<XmlNode>())
+            {
+                if (node.Name == "xml")
+                {
+                    // TODO: Validate header
+                }
+                else
+                {
+                    yield return ParseChildNode(node);
+                }
+            }
+        }
+
+        public Control Parse(string filePath)
+        {
+            var d = new XmlDocument();
+            d.Load(filePath);
+            return ParseChildNodes(d.ChildNodes).LastOrDefault() as Control;
+        }
+    }
+}
---
+++ Source/Tools/ContentExplorer/MainGame.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.BitmapFonts;
+using MonoGame.Extended.Gui;
+using MonoGame.Extended.ViewportAdapters;
+
+namespace ContentExplorer
+{
+    public class MainGame : Game
+    {
+        // ReSharper disable once NotAccessedField.Local
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+        private GuiSystem _guiSystem;
+
+        public MainGame()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            Window.ClientSizeChanged += WindowOnClientSizeChanged;
+        }
+
+        private void WindowOnClientSizeChanged(object sender, EventArgs eventArgs)
+        {
+            _guiSystem.ClientSizeChanged();
+        }
+
+        protected override void LoadContent()
+        {
+            var viewportAdapter = new DefaultViewportAdapter(GraphicsDevice);
+            var guiRenderer = new GuiSpriteBatchRenderer(GraphicsDevice, () => Matrix.Identity);
+            var font = Content.Load<BitmapFont>("Sensation");
+            BitmapFont.UseKernings = false;
+            Skin.CreateDefault(font);
+
+            var parser = new GuiMarkupParser();
+
+            var demoScreen = new Screen
+            {
+                Content = parser.Parse("Features/Example/MyScreen.mgeml")
+            };
+
+            _guiSystem = new GuiSystem(viewportAdapter, guiRenderer) { ActiveScreen = demoScreen };
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
+                Exit();
+
+            _guiSystem.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            _guiSystem.Draw(gameTime);
+        }
+    }
+}
