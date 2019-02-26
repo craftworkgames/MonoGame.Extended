77893d7e522516f1fc1f10fca18527c356d891dd
ryancheung <ryancheung.go@gmail.com>
2019-02-26 10:02:58 +0800

    Let MouseButton enum can be used as bit flags, as the System.Windows.Forms.MouseButton do

--- Source/MonoGame.Extended.Input/MouseButton.cs
+++ Source/MonoGame.Extended.Input/MouseButton.cs
@@ -5,11 +5,11 @@ namespace MonoGame.Extended.Input
     [Flags]
     public enum MouseButton
     {
-        None,
-        Left,
-        Middle,
-        Right,
-        XButton1,
-        XButton2
+        None = 0,
+        Left = 1048576,
+        Middle = 4194304,
+        Right = 2097152,
+        XButton1 = 8388608,
+        XButton2 = 16777216
     }
 }
