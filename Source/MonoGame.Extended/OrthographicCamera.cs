5040a2512e65ef80cfb02a68fb0bea5515a2d984
Dylan Wilson <dylan@craftworkgames.com>
2019-05-27 23:04:12 +1000

    refactored the camera to have a base class

--- Source/MonoGame.Extended/OrthographicCamera.cs
+++ Source/MonoGame.Extended/OrthographicCamera.cs
@@ -5,11 +5,35 @@ using MonoGame.Extended.ViewportAdapters;
 
 namespace MonoGame.Extended
 {
-    public abstract class Camera
+    public abstract class Camera<T> where T : struct
     {
+        public abstract T Position { get; set; }
+        public abstract float Rotation { get; set; }
+        public abstract float Zoom { get; set; }
+        public abstract float MinimumZoom { get; set; }
+        public abstract float MaximumZoom { get; set; }
+        public abstract RectangleF BoundingRectangle { get; }
+        public abstract T Origin { get; set; }
+        public abstract T Center { get; }
+
+        public abstract void Move(T direction);
+        public abstract void Rotate(float deltaRadians);
+        public abstract void ZoomIn(float deltaZoom);
+        public abstract void ZoomOut(float deltaZoom);
+        public abstract void LookAt(T position);
+
+        public abstract T WorldToScreen(T worldPosition);
+        public abstract T ScreenToWorld(T screenPosition);
+
+        public abstract Matrix GetViewMatrix();
+        public abstract Matrix GetInverseViewMatrix();
+
+        public abstract BoundingFrustum GetBoundingFrustum();
+        public abstract ContainmentType Contains(Vector2 vector2);
+        public abstract ContainmentType Contains(Rectangle rectangle);
     }
 
-    public class OrthographicCamera : Camera, IMovable, IRotatable
+    public sealed class OrthographicCamera : Camera<Vector2>, IMovable, IRotatable
     {
         private readonly ViewportAdapter _viewportAdapter;
         private float _maximumZoom = float.MaxValue;
@@ -31,9 +55,12 @@ namespace MonoGame.Extended
             Position = Vector2.Zero;
         }
 
-        public Vector2 Origin { get; set; }
+        public override Vector2 Position { get; set; }
+        public override float Rotation { get; set; }
+        public override Vector2 Origin { get; set; }
+        public override Vector2 Center => Position + Origin;
 
-        public float Zoom
+        public override float Zoom
         {
             get => _zoom;
             set
@@ -45,7 +72,7 @@ namespace MonoGame.Extended
             }
         }
 
-        public float MinimumZoom
+        public override float MinimumZoom
         {
             get => _minimumZoom;
             set
@@ -60,7 +87,7 @@ namespace MonoGame.Extended
             }
         }
 
-        public float MaximumZoom
+        public override float MaximumZoom
         {
             get => _maximumZoom;
             set
@@ -75,7 +102,7 @@ namespace MonoGame.Extended
             }
         }
 
-        public RectangleF BoundingRectangle
+        public override RectangleF BoundingRectangle
         {
             get
             {
@@ -88,26 +115,23 @@ namespace MonoGame.Extended
                 return new RectangleF(topLeft.X, topLeft.Y, width, height);
             }
         }
-
-        public Vector2 Position { get; set; }
-        public float Rotation { get; set; }
-
-        public void Move(Vector2 direction)
+        
+        public override void Move(Vector2 direction)
         {
             Position += Vector2.Transform(direction, Matrix.CreateRotationZ(-Rotation));
         }
 
-        public void Rotate(float deltaRadians)
+        public override void Rotate(float deltaRadians)
         {
             Rotation += deltaRadians;
         }
 
-        public void ZoomIn(float deltaZoom)
+        public override void ZoomIn(float deltaZoom)
         {
             ClampZoom(Zoom + deltaZoom);
         }
 
-        public void ZoomOut(float deltaZoom)
+        public override void ZoomOut(float deltaZoom)
         {
             ClampZoom(Zoom - deltaZoom);
         }
@@ -120,7 +144,7 @@ namespace MonoGame.Extended
                 Zoom = value > MaximumZoom ? MaximumZoom : value;
         }
 
-        public void LookAt(Vector2 position)
+        public override void LookAt(Vector2 position)
         {
             Position = position - new Vector2(_viewportAdapter.VirtualWidth/2f, _viewportAdapter.VirtualHeight/2f);
         }
@@ -130,7 +154,7 @@ namespace MonoGame.Extended
             return WorldToScreen(new Vector2(x, y));
         }
 
-        public Vector2 WorldToScreen(Vector2 worldPosition)
+        public override Vector2 WorldToScreen(Vector2 worldPosition)
         {
             var viewport = _viewportAdapter.Viewport;
             return Vector2.Transform(worldPosition + new Vector2(viewport.X, viewport.Y), GetViewMatrix());
@@ -141,7 +165,7 @@ namespace MonoGame.Extended
             return ScreenToWorld(new Vector2(x, y));
         }
 
-        public Vector2 ScreenToWorld(Vector2 screenPosition)
+        public override Vector2 ScreenToWorld(Vector2 screenPosition)
         {
             var viewport = _viewportAdapter.Viewport;
             return Vector2.Transform(screenPosition - new Vector2(viewport.X, viewport.Y),
@@ -168,25 +192,24 @@ namespace MonoGame.Extended
             return GetVirtualViewMatrix(Vector2.One);
         }
 
-        public Matrix GetViewMatrix()
+        public override Matrix GetViewMatrix()
         {
             return GetViewMatrix(Vector2.One);
         }
 
-        public Matrix GetInverseViewMatrix()
+        public override Matrix GetInverseViewMatrix()
         {
             return Matrix.Invert(GetViewMatrix());
         }
 
         private Matrix GetProjectionMatrix(Matrix viewMatrix)
         {
-            var projection = Matrix.CreateOrthographicOffCenter(0, _viewportAdapter.VirtualWidth,
-                _viewportAdapter.VirtualHeight, 0, -1, 0);
+            var projection = Matrix.CreateOrthographicOffCenter(0, _viewportAdapter.VirtualWidth, _viewportAdapter.VirtualHeight, 0, -1, 0);
             Matrix.Multiply(ref viewMatrix, ref projection, out projection);
             return projection;
         }
 
-        public BoundingFrustum GetBoundingFrustum()
+        public override BoundingFrustum GetBoundingFrustum()
         {
             var viewMatrix = GetVirtualViewMatrix();
             var projectionMatrix = GetProjectionMatrix(viewMatrix);
@@ -198,12 +221,12 @@ namespace MonoGame.Extended
             return Contains(point.ToVector2());
         }
 
-        public ContainmentType Contains(Vector2 vector2)
+        public override ContainmentType Contains(Vector2 vector2)
         {
             return GetBoundingFrustum().Contains(new Vector3(vector2.X, vector2.Y, 0));
         }
 
-        public ContainmentType Contains(Rectangle rectangle)
+        public override ContainmentType Contains(Rectangle rectangle)
         {
             var max = new Vector3(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height, 0.5f);
             var min = new Vector3(rectangle.X, rectangle.Y, 0.5f);
