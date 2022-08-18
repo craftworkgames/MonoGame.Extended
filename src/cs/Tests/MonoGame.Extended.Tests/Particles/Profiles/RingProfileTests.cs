b426c86fa990801494718352ce7d172906cc6dab
Andreas Torebring <andreas.torebring@gmail.com>
2022-08-18 21:37:58 +0200

    Uncomment and fix tests

--- src/cs/Tests/MonoGame.Extended.Tests/Particles/Profiles/RingProfileTests.cs
+++ src/cs/Tests/MonoGame.Extended.Tests/Particles/Profiles/RingProfileTests.cs
@@ -1,42 +1,38 @@
-﻿//using System;
-//using Microsoft.Xna.Framework;
-//using MonoGame.Extended.Particles.Profiles;
-//using Xunit;
+﻿using System;
+using MonoGame.Extended.Particles.Profiles;
+using Xunit;
 
-//namespace MonoGame.Extended.Tests.Particles.Profiles
-//{
-//    
-//    public class RingProfileTests
-//    {
-//        [Fact]
-//        public void ReturnsOffsetEqualToRadius()
-//        {
-//            var subject = new RingProfile
-//            {
-//                Radius = 10f
-//            };
-//            Vector2 offset, heading;
-//            subject.GetOffsetAndHeading(out offset, out heading);
+namespace MonoGame.Extended.Tests.Particles.Profiles
+{
+    public class RingProfileTests
+    {
+        [Fact]
+        public void ReturnsOffsetEqualToRadius()
+        {
+            var subject = new RingProfile
+            {
+                Radius = 10f
+            };
+            subject.GetOffsetAndHeading(out var offset, out _);
 
-//            var length = Math.Sqrt(offset.X * offset.X + offset.Y * offset.Y);
-//            Assert.Equal(10f, length, 0.000001);
-//        }
+            var length = Math.Sqrt(offset.X * offset.X + offset.Y * offset.Y);
+            Assert.Equal(10f, length, 6);
+        }
 
-//        [Fact]
-//        public void WhenRadiateIsTrue_HeadingIsEqualToNormalizedOffset()
-//        {
-//            var subject = new RingProfile
-//            {
-//                Radius = 10f,
-//                Radiate = Profile.CircleRadiation.Out
-//            };
-//            Vector2 offset, heading;
-//            subject.GetOffsetAndHeading(out offset, out heading);
+        [Fact]
+        public void WhenRadiateIsTrue_HeadingIsEqualToNormalizedOffset()
+        {
+            var subject = new RingProfile
+            {
+                Radius = 10f,
+                Radiate = Profile.CircleRadiation.Out
+            };
+            subject.GetOffsetAndHeading(out var offset, out var heading);
 
-//            Assert.Equal(heading.X, offset.X / 10, 0.000001);
-//            Assert.Equal(heading.Y, offset.Y / 10, 0.000001);
+            Assert.Equal(heading.X, offset.X / 10, 6);
+            Assert.Equal(heading.Y, offset.Y / 10, 6);
 
-//        }
+        }
 
-//    }
-//}
+    }
+}
---
+++ src/cs/Tests/MonoGame.Extended.Tests/WithinDeltaEqualityComparer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoGame.Extended.Tests;
+
+public class WithinDeltaEqualityComparer : IEqualityComparer<float>
+{
+    private readonly float _delta;
+
+    public WithinDeltaEqualityComparer(float delta)
+    {
+        _delta = delta;
+    }
+
+    public bool Equals(float x, float y)
+    {
+        return Math.Abs(x - y) < _delta;
+    }
+
+    public int GetHashCode(float obj)
+    {
+        return obj.GetHashCode();
+    }
+}
