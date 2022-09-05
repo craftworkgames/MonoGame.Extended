3cf076349b0a0baf5a9a2b45583d3d31b9613bd2
Andreas Torebring <andreas.torebring@gmail.com>
2022-09-05 19:24:24 +0200

    Add OrientedBoundingRectangle type

---
+++ src/cs/MonoGame.Extended/Math/OrientedBoundingRectangle.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Xna.Framework;
+
+namespace MonoGame.Extended
+{
+    // Real-Time Collision Detection, Christer Ericson, 2005. Chapter 4.4; Bounding Volumes - Oriented Bounding Boxes (OBBs), pg 101.
+
+    /// <summary>
+    /// An oriented bounding rectangle is a rectangular block, much like a bounding rectangle
+    /// <see cref="BoundingRectangle" /> but with an arbitrary orientation <see cref="Vector2" />.
+    /// </summary>
+    /// <seealso cref="IEquatable{T}" />
+    [DebuggerDisplay($"{{{nameof(DebugDisplayString)},nq}}")]
+    public struct OrientedBoundingRectangle : IEquatable<OrientedBoundingRectangle>
+    {
+        /// <summary>
+        /// The centre position of this <see cref="OrientedBoundingRectangle" />.
+        /// </summary>
+        public Point2 Center;
+
+        /// <summary>
+        /// The distance from the <see cref="Center" /> point along both axes to any point on the boundary of this
+        /// <see cref="OrientedBoundingRectangle" />.
+        /// </summary>
+        ///
+        public Vector2 Radii;
+
+        /// <summary>
+        /// The rotation matrix <see cref="Matrix2" /> of the bounding rectangle <see cref="OrientedBoundingRectangle" />.
+        /// </summary>
+        public Matrix2 Orientation;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BoundingRectangle" /> structure from the specified centre
+        /// <see cref="Point2" /> and the radii <see cref="Size2" />.
+        /// </summary>
+        /// <param name="center">The centre <see cref="Point2" />.</param>
+        /// <param name="radii">The radii <see cref="Vector2" />.</param>
+        /// <param name="orientation">The orientation <see cref="Matrix2" />.</param>
+        public OrientedBoundingRectangle(Point2 center, Size2 radii, Matrix2 orientation)
+        {
+            Center = center;
+            Radii = radii;
+            Orientation = orientation;
+        }
+
+        /// <summary>
+        /// Computes the <see cref="OrientedBoundingRectangle"/> from the specified <paramref name="rectangle"/>
+        /// transformed by <paramref name="transformMatrix"/>.
+        /// </summary>
+        /// <param name="rectangle">The <see cref="OrientedBoundingRectangle"/> to transform.</param>
+        /// <param name="transformMatrix">The <see cref="Matrix2"/> transformation.</param>
+        /// <returns>A new <see cref="OrientedBoundingRectangle"/>.</returns>
+        public static OrientedBoundingRectangle Transform(OrientedBoundingRectangle rectangle, ref Matrix2 transformMatrix)
+        {
+            Transform(ref rectangle, ref transformMatrix, out var result);
+            return result;
+        }
+
+        private static void Transform(ref OrientedBoundingRectangle rectangle, ref Matrix2 transformMatrix, out OrientedBoundingRectangle result)
+        {
+            PrimitivesHelper.TransformOrientedBoundingRectangle(
+                ref rectangle.Center,
+                ref rectangle.Radii,
+                ref rectangle.Orientation,
+                ref transformMatrix);
+            result.Center = rectangle.Center;
+            result.Radii = rectangle.Radii;
+            result.Orientation = rectangle.Orientation;
+        }
+
+        /// <summary>
+        /// Compares to two <see cref="OrientedBoundingRectangle"/> structures. The result specifies whether the
+        /// the values of the <see cref="Center"/>, <see cref="Radii"/> and <see cref="Orientation"/> are
+        /// equal.
+        /// </summary>
+        /// <param name="left">The left <see cref="OrientedBoundingRectangle" />.</param>
+        /// <param name="right">The right <see cref="OrientedBoundingRectangle" />.</param>
+        /// <returns><c>true</c> if left and right argument are equal; otherwise, <c>false</c>.</returns>
+        public static bool operator ==(OrientedBoundingRectangle left, OrientedBoundingRectangle right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Compares to two <see cref="OrientedBoundingRectangle"/> structures. The result specifies whether the
+        /// the values of the <see cref="Center"/>, <see cref="Radii"/> or <see cref="Orientation"/> are
+        /// unequal.
+        /// </summary>
+        /// <param name="left">The left <see cref="OrientedBoundingRectangle" />.</param>
+        /// <param name="right">The right <see cref="OrientedBoundingRectangle" />.</param>
+        /// <returns><c>true</c> if left and right argument are unequal; otherwise, <c>false</c>.</returns>
+        public static bool operator !=(OrientedBoundingRectangle left, OrientedBoundingRectangle right)
+        {
+            return !left.Equals(right);
+        }
+
+        /// <summary>
+        /// Determines whether two instances of <see cref="OrientedBoundingRectangle"/> are equal.
+        /// </summary>
+        /// <param name="other">The other <see cref="OrientedBoundingRectangle"/>.</param>
+        /// <returns><c>true</c> if the specified <see cref="OrientedBoundingRectangle"/> is equal
+        /// to the current <see cref="OrientedBoundingRectangle"/>; otherwise, <c>false</c>.</returns>
+        public bool Equals(OrientedBoundingRectangle other)
+        {
+            return Center.Equals(other.Center) && Radii.Equals(other.Radii) && Orientation.Equals(other.Orientation);
+        }
+
+        /// <summary>
+        /// Determines whether two instances of <see cref="OrientedBoundingRectangle"/> are equal.
+        /// </summary>
+        /// <param name="obj">The <see cref="OrientedBoundingRectangle"/> to compare to.</param>
+        /// <returns><c>true</c> if the specified <see cref="OrientedBoundingRectangle"/> is equal
+        /// to the current <see cref="OrientedBoundingRectangle"/>; otherwise, <c>false</c>.</returns>
+        public override bool Equals(object obj)
+        {
+            return obj is OrientedBoundingRectangle other && Equals(other);
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Center, Radii, Orientation);
+        }
+
+        /// <summary>
+        /// Returns a <see cref="string" /> that represents this <see cref="OrientedBoundingRectangle" />.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string" /> that represents this <see cref="OrientedBoundingRectangle" />.
+        /// </returns>
+        public override string ToString()
+        {
+            return $"Centre: {Center}, Radii: {Radii}, Orientation: {Orientation}";
+        }
+
+        internal string DebugDisplayString => ToString();
+    }
+}
---
+++ src/cs/Tests/MonoGame.Extended.Tests/Primitives/OrientedBoundingRectangleTests.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using Microsoft.Xna.Framework;
+using Xunit;
+using Vector2 = Microsoft.Xna.Framework.Vector2;
+
+namespace MonoGame.Extended.Tests.Primitives;
+
+public class OrientedBoundingRectangleTests
+{
+    [Fact]
+    public void Initializes_oriented_bounding_rectangle()
+    {
+        var rect = new OrientedBoundingRectangle(new Point2(1, 2), new Size2(3, 4), new Matrix2(5, 6, 7, 8, 9, 10));
+
+        Assert.Equal(new Point2(1, 2), rect.Center);
+        Assert.Equal(new Vector2(3, 4), rect.Radii);
+        Assert.Equal(new Matrix2(5, 6, 7, 8, 9, 10), rect.Orientation);
+    }
+
+    public static readonly IEnumerable<object[]> _equalsComparisons = new[]
+        {
+            new object[]
+                {
+                    "empty compared with empty is true",
+                    new OrientedBoundingRectangle(Point2.Zero, Size2.Empty, Matrix2.Identity),
+                    new OrientedBoundingRectangle(Point2.Zero, Size2.Empty, Matrix2.Identity)
+                },
+            new object[]
+                {
+                    "initialized compared with initialized true",
+                    new OrientedBoundingRectangle(new Point2(1, 2), new Size2(3, 4), new Matrix2(5, 6, 7, 8, 9, 10)),
+                    new OrientedBoundingRectangle(new Point2(1, 2), new Size2(3, 4), new Matrix2(5, 6, 7, 8, 9, 10))
+                }
+        };
+
+    [Theory]
+    [MemberData(nameof(_equalsComparisons))]
+#pragma warning disable xUnit1026
+    public void Equals_comparison(string name, OrientedBoundingRectangle first, OrientedBoundingRectangle second)
+#pragma warning restore xUnit1026
+    {
+        Assert.True(first == second);
+        Assert.False(first != second);
+    }
+
+    public class Transform
+    {
+        [Fact]
+        public void Center_point_is_not_translated()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(1, 2), new Size2(), new Matrix2());
+            var transform = Matrix2.Identity;
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Point2(1, 2), result.Center);
+        }
+
+        [Fact]
+        public void Center_point_is_translated()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(0, 0), new Size2(), new Matrix2());
+            var transform = Matrix2.CreateTranslation(1, 2);
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Point2(1, 2), result.Center);
+        }
+
+        [Fact]
+        public void Radii_is_not_changed_by_identity_transform()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(), new Size2(10, 20), new Matrix2());
+            var transform = Matrix2.Identity;
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Vector2(10, 20), result.Radii);
+        }
+
+        [Fact]
+        public void Radii_is_not_changed_by_translation()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(1, 2), new Size2(10, 20), new Matrix2());
+            var transform = Matrix2.CreateTranslation(1, 2);
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Vector2(10, 20), result.Radii);
+        }
+
+        [Fact]
+        public void Rotate_45_degrees_left()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(), new Size2(), Matrix2.Identity);
+            var transform = Matrix2.CreateRotationZ(MathHelper.PiOver4);
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Point2(), result.Center);
+            Assert.Equal(new Vector2(), result.Radii);
+            Assert.Equal(Matrix2.CreateRotationZ(MathHelper.PiOver4), result.Orientation);
+        }
+
+        [Fact]
+        public void Rotate_to_45_degrees_from_180()
+        {
+            var rectangle = new OrientedBoundingRectangle(new Point2(), new Size2(), Matrix2.CreateRotationZ(MathHelper.Pi));
+            var transform = Matrix2.CreateRotationZ(-3 * MathHelper.PiOver4);
+            var expectedOrientation = Matrix2.CreateRotationZ(MathHelper.PiOver4);
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(new Point2(), result.Center);
+            Assert.Equal(new Vector2(), result.Radii);
+            Assert.Equal(expectedOrientation.M11, result.Orientation.M11, 6);
+            Assert.Equal(expectedOrientation.M12, result.Orientation.M12, 6);
+            Assert.Equal(expectedOrientation.M21, result.Orientation.M21, 6);
+            Assert.Equal(expectedOrientation.M22, result.Orientation.M22, 6);
+            Assert.Equal(expectedOrientation.M31, result.Orientation.M31, 6);
+            Assert.Equal(expectedOrientation.M32, result.Orientation.M32, 6);
+        }
+
+        [Fact]
+        public void Applies_rotation_and_translation()
+        {
+            /*   Rectangle with center point p, aligned in coordinate system with origin 0.
+             *
+             *                    :
+             *                    :
+             *                    +-+
+             *                    | |
+             *                    |p|
+             *                    | |
+             *     ...............0-+............
+             *                    :
+             *                    :
+             *                    :
+             *
+             *    Rotate around center point p, 90 degrees around origin 0.
+             *
+             *                    :
+             *                    :
+             *                +---+
+             *                | p |
+             *     ...........+---0..............
+             *                    :
+             *                    :
+             *                    :
+             *
+             *    Then translate rectangle by x=10 and y=20.
+             *                    :
+             *                    :     +---+
+             *                    :     | p |
+             *    y=21 - - - - - - - -> +---+
+             *                    .
+             *                    :
+             *    ...............0..............
+             *                    :
+             *                    :
+             *                    :
+             */
+            var rectangle = new OrientedBoundingRectangle(new Point2(1, 2), new Size2(2, 4), Matrix2.Identity);
+            var transform =
+                Matrix2.CreateRotationZ(MathHelper.PiOver2)
+                *
+                Matrix2.CreateTranslation(10, 20);
+
+            var result = OrientedBoundingRectangle.Transform(rectangle, ref transform);
+
+            Assert.Equal(-2 + 10, result.Center.X, 6);
+            Assert.Equal(1 + 20, result.Center.Y, 6);
+            Assert.Equal(4, result.Radii.X, 6);
+            Assert.Equal(2, result.Radii.Y, 6);
+            Assert.Equal(Matrix2.CreateRotationZ(MathHelper.PiOver2), result.Orientation);
+        }
+    }
+}
