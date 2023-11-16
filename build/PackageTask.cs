3ae6aa4b14ca21d4e2f7c800abd3036bb1e5f6ed
Christopher Whitley <aristurtledev@gmail.com>
2023-11-16 13:32:50 -0500

    Remove async, ignore NU5118 on package

--- build/PackageTask.cs
+++ build/PackageTask.cs
@@ -1,3 +1,4 @@
+using Cake.Common.Build;
 using Cake.Common.IO;
 using Cake.Common.Tools.DotNet;
 using Cake.Common.Tools.DotNet.MSBuild;
@@ -10,13 +11,16 @@ namespace BuildScripts;
 [TaskName(nameof(PackageTask))]
 public sealed class PackageTask : FrostingTask<BuildContext>
 {
-    public override async void Run(BuildContext context)
+    public override void Run(BuildContext context)
     {
         context.CleanDirectories(context.ArtifactsDirectory);
         context.CreateDirectory(context.ArtifactsDirectory);
 
         DotNetMSBuildSettings msBuildSettings = new DotNetMSBuildSettings();
         msBuildSettings.WithProperty("Version", context.Version);
+
+        //  Ignore warnings about adding duplicate items added to package
+        msBuildSettings.WithProperty("NoWarn", "NU5118")
 
         DotNetPackSettings packSettings = new DotNetPackSettings()
         {
