599f043d103f7c215486d04de31942afcf162862
Christopher Whitley <aristurtledev@gmail.com>
2023-11-16 00:48:54 -0500

    Added BuildTask

---
+++ build/BuildTask.cs
@@ -0,0 +1,24 @@
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Build;
+using Cake.Common.Tools.DotNet.MSBuild;
+using Cake.Frosting;
+
+namespace BuildScripts;
+
+[TaskName(nameof(BuildTask))]
+public sealed class BuildTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        DotNetMSBuildSettings msBuildSettings = new DotNetMSBuildSettings();
+        msBuildSettings.WithProperty("Version", context.Version);
+
+        DotNetBuildSettings buildSettings = new DotNetBuildSettings()
+        {
+            MSBuildSettings = msBuildSettings,
+            Configuration = "Release",
+        };
+
+        context.DotNetBuild("./MonoGame.Extended.sln", buildSettings);
+    }
+}
