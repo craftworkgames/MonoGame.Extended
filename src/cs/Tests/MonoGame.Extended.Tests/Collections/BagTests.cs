e1d280b3565940d841981906566eb616a0314b27
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2023-11-16 01:49:35 -0500

    Forgot to add System.Runtime using statement

--- src/cs/Tests/MonoGame.Extended.Tests/Collections/BagTests.cs
+++ src/cs/Tests/MonoGame.Extended.Tests/Collections/BagTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Runtime;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Xunit;
