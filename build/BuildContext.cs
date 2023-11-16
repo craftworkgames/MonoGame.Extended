1e58c09e08b01ec45aa65b9da0cc4f7b50874a27
Christopher Whitley <aristurtledev@gmail.com>
2023-11-16 00:48:05 -0500

    Added BuildContext

---
+++ build/BuildContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using Cake.Common;
+using Cake.Common.Build;
+using Cake.Common.Xml;
+using Cake.Core;
+using Cake.Frosting;
+
+namespace BuildScripts;
+
+public sealed class BuildContext : FrostingContext
+{
+    public string ArtifactsDirectory { get; }
+    public string Version { get; }
+
+    public BuildContext(ICakeContext context) : base(context)
+    {
+        ArtifactsDirectory = context.Arguments(nameof(ArtifactsDirectory), "artifacts").FirstOrDefault();
+        Version = context.XmlPeek("Directory.Build.targets", "/Project/PropertyGroup/Version");
+
+        if (context.BuildSystem().IsRunningOnGitHubActions)
+        {
+            Version += "." + context.EnvironmentVariable("GITHUB_RUN_NUMBER");
+        }
+    }
+}
