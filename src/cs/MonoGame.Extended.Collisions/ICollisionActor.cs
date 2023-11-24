b24933b1b7e2e7ab42abacfcae30168bc444de10
Gandifil <gandifilmk@gmail.com>
2023-11-25 02:32:37 +0300

    Add comments for collision actor.

--- src/cs/MonoGame.Extended.Collisions/ICollisionActor.cs
+++ src/cs/MonoGame.Extended.Collisions/ICollisionActor.cs
@@ -7,9 +7,21 @@ namespace MonoGame.Extended.Collisions
     /// </summary>
     public interface ICollisionActor
     {
+        /// <summary>
+        /// A name of layer, which will contains this actor.
+        /// If it equals null, an actor will insert into a default layer
+        /// </summary>
         string LayerName { get => null; }
+
+        /// <summary>
+        /// A bounds of an actor. It is using for collision calculating
+        /// </summary>
         IShapeF Bounds { get; }
 
+        /// <summary>
+        /// It will called, when collision with an another actor fires
+        /// </summary>
+        /// <param name="collisionInfo">Data about collision</param>
         void OnCollision(CollisionEventArgs collisionInfo);
     }
 }
