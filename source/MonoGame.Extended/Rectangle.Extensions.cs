ddef65254c3f92e1dba2ef7a2000769eb75908e4
Christopher Whitley <aristurtledev@gmail.com>
2024-06-11 19:21:42 -0400

    Added `GetRelativeRectangle` method

--- source/MonoGame.Extended/Rectangle.Extensions.cs
+++ source/MonoGame.Extended/Rectangle.Extensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 
 namespace MonoGame.Extended
 {
@@ -33,7 +33,26 @@ namespace MonoGame.Extended
             if (rectangle.Width <= 0 || rectangle.Height <= 0)
                 return Rectangle.Empty;
 
-            return rectangle;
-        }
+    /// <summary>
+    /// Gets a rectangle that is relative to the specified source rectangle, with the specified offsets and dimensions.
+    /// </summary>
+    /// <param name="source">The source rectangle.</param>
+    /// <param name="x">The x-coordinate of the relative rectangle, relative to the source rectangle.</param>
+    /// <param name="y">The y-coordinate of the relative rectangle, relative to the source rectangle.</param>
+    /// <param name="width">The width, in pixels, of the relative rectangle.</param>
+    /// <param name="height">The height, in pixels, of the relative rectangle.</param>
+    /// <returns>The relative rectangle, clipped to the source rectangle.</returns>
+    public static Rectangle GetRelativeRectangle(Rectangle source, int x, int y, int width, int height)
+    {
+        int absoluteX = source.X + x;
+        int absoluteY = source.Y + y;
+
+        Rectangle relative;
+        relative.X = MathHelper.Clamp(absoluteX, source.Left, source.Right);
+        relative.Y = MathHelper.Clamp(absoluteY, source.Top, source.Bottom);
+        relative.Width = Math.Max(Math.Min(absoluteX + width, source.Right) - relative.X, 0);
+        relative.Height = Math.Max(Math.Min(absoluteY + height, source.Bottom) - relative.Y, 0);
+
+        return relative;
     }
 }
