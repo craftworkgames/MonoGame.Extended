30f44cd6498cf5cf9c4583131f39276160df17b7
Christopher Whitley <aristurtledev@gmail.com>
2024-06-11 19:37:44 -0400

    Top-level namespace

--- source/MonoGame.Extended/Graphics/TextureRegion.cs
+++ source/MonoGame.Extended/Graphics/TextureRegion.cs
@@ -2,53 +2,52 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace MonoGame.Extended.Graphics
+namespace MonoGame.Extended.Graphics;
+
+public class TextureRegion
 {
-    public class TextureRegion
+    public string Name { get; }
+    public Texture2D Texture { get; protected set; }
+    public int X { get; }
+    public int Y { get; }
+    public int Width { get; }
+    public int Height { get; }
+    public SizeF Size => new SizeF(Width, Height);
+    public object Tag { get; set; }
+    public Rectangle Bounds => new Rectangle(X, Y, Width, Height);
+
+    public TextureRegion(Texture2D texture, int x, int y, int width, int height)
+        : this(null, texture, x, y, width, height)
     {
-        public string Name { get; }
-        public Texture2D Texture { get; protected set; }
-        public int X { get; }
-        public int Y { get; }
-        public int Width { get; }
-        public int Height { get; }
-        public SizeF Size => new SizeF(Width, Height);
-        public object Tag { get; set; }
-        public Rectangle Bounds => new Rectangle(X, Y, Width, Height);
+    }
 
-        public TextureRegion(Texture2D texture, int x, int y, int width, int height)
-            : this(null, texture, x, y, width, height)
-        {
-        }
+    public TextureRegion(Texture2D texture, Rectangle region)
+        : this(null, texture, region.X, region.Y, region.Width, region.Height)
+    {
+    }
 
-        public TextureRegion(Texture2D texture, Rectangle region)
-            : this(null, texture, region.X, region.Y, region.Width, region.Height)
-        {
-        }
+    public TextureRegion(string name, Texture2D texture, Rectangle region)
+        : this(name, texture, region.X, region.Y, region.Width, region.Height)
+    {
+    }
 
-        public TextureRegion(string name, Texture2D texture, Rectangle region)
-            : this(name, texture, region.X, region.Y, region.Width, region.Height)
-        {
-        }
+    public TextureRegion(Texture2D texture)
+        : this(texture.Name, texture, 0, 0, texture.Width, texture.Height)
+    {
+    }
 
-        public TextureRegion(Texture2D texture)
-            : this(texture.Name, texture, 0, 0, texture.Width, texture.Height)
-        {
-        }
+    public TextureRegion(string name, Texture2D texture, int x, int y, int width, int height)
+    {
+        Name = name;
+        Texture = texture;
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
 
-        public TextureRegion(string name, Texture2D texture, int x, int y, int width, int height)
-        {
-            Name = name;
-            Texture = texture;
-            X = x;
-            Y = y;
-            Width = width;
-            Height = height;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name ?? string.Empty} {Bounds}";
-        }
+    public override string ToString()
+    {
+        return $"{Name ?? string.Empty} {Bounds}";
     }
 }
