37b72816a114d29e36766c047353654de61c8d7c
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:10:52 -0400

    Move properties before constructor.

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
@@ -10,14 +10,16 @@ namespace MonoGame.Extended.Graphics
     [DebuggerDisplay("{Index} {Duration}")]
     public class SpriteSheetAnimationFrame
     {
+        public int Index { get; set; }
+        public float Duration { get; set; }
+
         public SpriteSheetAnimationFrame(int index, float duration = 0.2f)
         {
             Index = index;
             Duration = duration;
         }
 
-        public int Index { get; set; }
-        public float Duration { get; set; }
+
     }
 
     public class SpriteSheetAnimationFrameJsonConverter : JsonConverter<SpriteSheetAnimationFrame>
