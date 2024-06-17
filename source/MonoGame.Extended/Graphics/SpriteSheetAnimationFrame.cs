8d1a67e7d5f31b2bafc6e6dd561acff78467e0c5
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:18:01 -0400

    Make file-scoped namespace

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
@@ -3,26 +3,25 @@ using System.Diagnostics;
 using Microsoft.Xna.Framework.Graphics;
 
 
-namespace MonoGame.Extended.Graphics
+namespace MonoGame.Extended.Graphics;
+
+[DebuggerDisplay("{Index} {Duration}")]
+public class SpriteSheetAnimationFrame
 {
-    [DebuggerDisplay("{Index} {Duration}")]
-    public class SpriteSheetAnimationFrame
+    public int FrameIndex { get; }
+    public TimeSpan Duration { get; }
+
+    public Texture2DRegion TextureRegion { get; }
+
+    public SpriteSheetAnimationFrame(int index, Texture2DRegion region, TimeSpan duration)
     {
-        public int FrameIndex { get; }
-        public TimeSpan Duration { get; }
-
-        public Texture2DRegion TextureRegion { get; }
-
-        public SpriteSheetAnimationFrame(int index, Texture2DRegion region, TimeSpan duration)
+        ArgumentNullException.ThrowIfNull(region);
+        if (region.Texture.IsDisposed)
         {
-            ArgumentNullException.ThrowIfNull(region);
-            if (region.Texture.IsDisposed)
-            {
-                throw new ObjectDisposedException(nameof(region), $"The source {nameof(Texture2D)} of {nameof(region)} was disposed prior.");
-            }
-
-            FrameIndex = index;
-            Duration = duration;
+            throw new ObjectDisposedException(nameof(region), $"The source {nameof(Texture2D)} of {nameof(region)} was disposed prior.");
         }
+
+        FrameIndex = index;
+        Duration = duration;
     }
 }
