e6546ec4909a59604b70869f00364a44c253132f
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:17:08 -0400

    Remove `SpriteSheetAnimationFrameJsonConverter`

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
@@ -7,7 +7,6 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MonoGame.Extended.Graphics
 {
-    [JsonConverter(typeof(SpriteSheetAnimationFrameJsonConverter))]
     [DebuggerDisplay("{Index} {Duration}")]
     public class SpriteSheetAnimationFrame
     {
@@ -27,35 +26,5 @@ namespace MonoGame.Extended.Graphics
             FrameIndex = index;
             Duration = duration;
         }
-    }
-
-    public class SpriteSheetAnimationFrameJsonConverter : JsonConverter<SpriteSheetAnimationFrame>
-    {
-        /// <inheritdoc />
-        public override SpriteSheetAnimationFrame Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            switch (reader.TokenType)
-            {
-                case JsonTokenType.Number:
-                    var index = reader.GetInt32();
-                    return new SpriteSheetAnimationFrame(index, TimeSpan.FromSeconds(0.2f));
-
-                case JsonTokenType.StartObject:
-                    var frame = JsonSerializer.Deserialize<SpriteSheetAnimationFrame>(ref reader, options);
-                    return frame;
-
-                case JsonTokenType.Null:
-                    return null;
-
-                default:
-                    throw new JsonException();
-            }
-        }
-
-        public override void Write(Utf8JsonWriter writer, SpriteSheetAnimationFrame value, JsonSerializerOptions options)
-        {
-            ArgumentNullException.ThrowIfNull(writer);
-            JsonSerializer.Serialize(writer, value, options);
-        }
-    }
+    }   
 }
