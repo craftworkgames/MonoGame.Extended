7d13089524680c247135f9c8867dbc8c3dd9dcc4
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:23:53 -0400

    Added file header

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
@@ -1,3 +1,7 @@
+// Copyright (c) Craftwork Games. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE file in the project root for full license information.
+
 using System;
 using System.Diagnostics;
 using Microsoft.Xna.Framework.Graphics;
