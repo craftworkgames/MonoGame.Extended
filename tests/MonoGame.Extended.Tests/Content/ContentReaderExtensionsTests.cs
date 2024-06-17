f4e225856c61fa6541b96d6f8695e2f7fb6c8b03
Nikos Kastellanos <nkastellanos@gmail.com>
2024-06-18 01:18:42 +0300

    remove test for issue #633

--- tests/MonoGame.Extended.Tests/Content/ContentReaderExtensionsTests.cs
+++ tests/MonoGame.Extended.Tests/Content/ContentReaderExtensionsTests.cs
@@ -22,28 +22,5 @@ namespace MonoGame.Extended.Tests.Content
             Assert.True(ContentReaderExtensions.ShortenRelativePath(input) == expectedoutput);
         }
 
-        // Test added for issue #633
-        // https://github.com/craftworkgames/MonoGame.Extended/issues/633
-        //
-        // Issue states that the input string from a json file is parsed in correctly by the content reader.
-        // Adding this test to ensure that GetRelativeAssetName is parsed correctly.
-        [Theory]
-        [InlineData("test.tex.png", "test.tex")]
-        public void ContentReaderExtensions_GetRelativeAssetName(string input, string expected)
-        {
-            var contentReaderType = typeof(Microsoft.Xna.Framework.Content.ContentReader).GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic).First();
-            var contentReader = (Microsoft.Xna.Framework.Content.ContentReader)contentReaderType.Invoke(new object[]
-            {
-                null,                   // manager: parameter
-                new MemoryStream(),     // stream: parameter
-                $"{expected}.xnb",      // assetName: parameter
-                0,                      // version: parameter
-                null                    // recordDisposableObject: parameter
-            });
-
-            var actual = contentReader.GetRelativeAssetName(input);
-
-            Assert.Equal(expected, actual);
-        }
     }
 }
