b96acc4f3fe38adb5ea9bea03148778d5de5f219
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:14:33 -0400

    Make property get only

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationFrame.cs
@@ -11,15 +11,15 @@ namespace MonoGame.Extended.Graphics
     [DebuggerDisplay("{Index} {Duration}")]
     public class SpriteSheetAnimationFrame
     {
-        public int FrameIndex { get; set; }
-        public TimeSpan Duration { get; set; }
+        public int FrameIndex { get; }
+        public TimeSpan Duration { get; }
 
-        public Texture2DRegion TextureRegion { get;}
+        public Texture2DRegion TextureRegion { get; }
 
         public SpriteSheetAnimationFrame(int index, Texture2DRegion region, TimeSpan duration)
         {
             ArgumentNullException.ThrowIfNull(region);
-            if(region.Texture.IsDisposed)
+            if (region.Texture.IsDisposed)
             {
                 throw new ObjectDisposedException(nameof(region), $"The source {nameof(Texture2D)} of {nameof(region)} was disposed prior.");
             }
