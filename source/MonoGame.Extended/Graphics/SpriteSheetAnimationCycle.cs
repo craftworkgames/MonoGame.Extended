a8495821b3ee1730e18e416a0f75735d9a39b981
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 15:35:02 -0400

    File scoped namespace

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationCycle.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationCycle.cs
@@ -1,18 +1,17 @@
 using System.Collections.Generic;
 
-namespace MonoGame.Extended.Graphics
-{
-    public class SpriteSheetAnimationCycle
-    {
-        public SpriteSheetAnimationCycle()
-        {
-            Frames = new List<SpriteSheetAnimationFrame>();
-        }
+namespace MonoGame.Extended.Graphics;
 
-        public float FrameDuration { get; set; } = 0.2f;
-        public List<SpriteSheetAnimationFrame> Frames { get; set; }
-        public bool IsLooping { get; set; }
-        public bool IsReversed { get; set; }
-        public bool IsPingPong { get; set; }
+public class SpriteSheetAnimationCycle
+{
+    public SpriteSheetAnimationCycle()
+    {
+        Frames = new List<SpriteSheetAnimationFrame>();
     }
+
+    public float FrameDuration { get; set; } = 0.2f;
+    public List<SpriteSheetAnimationFrame> Frames { get; set; }
+    public bool IsLooping { get; set; }
+    public bool IsReversed { get; set; }
+    public bool IsPingPong { get; set; }
 }
