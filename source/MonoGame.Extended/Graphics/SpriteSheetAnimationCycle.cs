7b9e36ccb870526dfe99c580b4a91a0182bffe6f
Christopher Whitley <aristurtledev@gmail.com>
2024-06-17 21:24:33 -0400

    Reorganize: Move properties above constructor

--- source/MonoGame.Extended/Graphics/SpriteSheetAnimationCycle.cs
+++ source/MonoGame.Extended/Graphics/SpriteSheetAnimationCycle.cs
@@ -8,14 +8,14 @@ namespace MonoGame.Extended.Graphics;
 
 public class SpriteSheetAnimationCycle
 {
-    public SpriteSheetAnimationCycle()
-    {
-        Frames = new List<SpriteSheetAnimationFrame>();
-    }
-
     public float FrameDuration { get; set; } = 0.2f;
     public List<SpriteSheetAnimationFrame> Frames { get; set; }
     public bool IsLooping { get; set; }
     public bool IsReversed { get; set; }
     public bool IsPingPong { get; set; }
+    public SpriteSheetAnimationCycle()
+    {
+        Frames = new List<SpriteSheetAnimationFrame>();
+    }
+
 }
