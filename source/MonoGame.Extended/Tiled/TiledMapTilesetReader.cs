ca491e035a342deeff44875566ea498f16848f75
Christopher Whitley <aristurtledev@gmail.com>
2024-07-06 22:02:13 -0400

    Add Content namespace

--- source/MonoGame.Extended/Tiled/TiledMapTilesetReader.cs
+++ source/MonoGame.Extended/Tiled/TiledMapTilesetReader.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Content;
 using System;
 
 namespace MonoGame.Extended.Tiled
