88fd65d71028bd5c12e065aa278f69dce62f7f4a
Christopher Whitley <aristurtledev@gmail.com>
2024-07-06 22:01:46 -0400

    Cleanup namespace

--- source/MonoGame.Extended/Serialization/Json/TextureRegionService.cs
+++ source/MonoGame.Extended/Serialization/Json/TextureRegionService.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using MonoGame.Extended.Graphics;
-using MonoGame.Extended.TextureAtlases;
 
 namespace MonoGame.Extended.Serialization.Json
 {
