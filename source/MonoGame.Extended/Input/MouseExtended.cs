b0a45229f630e6447ef3991ab3d8b35e82c721b1
Christopher Whitley <aristurtledev@gmail.com>
2024-07-09 12:28:23 -0400

    file scoped namespace

--- source/MonoGame.Extended/Input/MouseExtended.cs
+++ source/MonoGame.Extended/Input/MouseExtended.cs
@@ -2,35 +2,34 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
-namespace MonoGame.Extended.Input
+namespace MonoGame.Extended.Input;
+
+public static class MouseExtended
 {
-    public static class MouseExtended
+    // TODO: This global static state was a horrible idea.
+    private static MouseState _currentMouseState;
+    private static MouseState _previousMouseState;
+
+    public static MouseStateExtended GetState()
     {
-        // TODO: This global static state was a horrible idea.
-        private static MouseState _currentMouseState;
-        private static MouseState _previousMouseState;
+        return new MouseStateExtended(_currentMouseState, _previousMouseState);
+    }
 
-        public static MouseStateExtended GetState()
-        {
-            return new MouseStateExtended(_currentMouseState, _previousMouseState);
-        }
+    public static void Refresh()
+    {
+        _previousMouseState = _currentMouseState;
+        _currentMouseState = Mouse.GetState();
+    }
 
-        public static void Refresh()
-        {
-            _previousMouseState = _currentMouseState;
-            _currentMouseState = Mouse.GetState();
-        }
-
-        public static void SetPosition(int x, int y) => Mouse.SetPosition(x, y);
-        public static void SetPosition(Point point) => Mouse.SetPosition(point.X, point.Y);
+    public static void SetPosition(int x, int y) => Mouse.SetPosition(x, y);
+    public static void SetPosition(Point point) => Mouse.SetPosition(point.X, point.Y);
 #if !FNA
-        public static void SetCursor(MouseCursor cursor) => Mouse.SetCursor(cursor);
+    public static void SetCursor(MouseCursor cursor) => Mouse.SetCursor(cursor);
 #endif
 
-        public static IntPtr WindowHandle
-        {
-            get => Mouse.WindowHandle;
-            set => Mouse.WindowHandle = value;
-        }
+    public static IntPtr WindowHandle
+    {
+        get => Mouse.WindowHandle;
+        set => Mouse.WindowHandle = value;
     }
 }
