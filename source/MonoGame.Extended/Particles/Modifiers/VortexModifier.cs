449fd82b2e08824108198317af7843ae2bad46fa
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2024-07-11 21:31:50 -0400

    Use MaxSpeed when calculating speedGain This calculation was never implemented from the Mercury Particle Engine port

--- source/MonoGame.Extended/Particles/Modifiers/VortexModifier.cs
+++ source/MonoGame.Extended/Particles/Modifiers/VortexModifier.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 
 namespace MonoGame.Extended.Particles.Modifiers
@@ -20,7 +21,8 @@ namespace MonoGame.Extended.Particles.Modifiers
 
                 var distance2 = diff.LengthSquared();
 
-                var speedGain = _gravConst*Mass/distance2*elapsedSeconds;
+                var speedGain = _gravConst*Mass/distance2;
+                speedGain = Math.Max(Math.Min(speedGain, MaxSpeed), -MaxSpeed) * elapsedSeconds;
                 // normalize distances and multiply by speedGain
                 diff.Normalize();
                 particle->Velocity += diff*speedGain;
