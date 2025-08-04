eefa1ac8d7be78a8df9693dcc1a12ee31703cd9d
Joachim Grill <joachim@codeandweb.com>
2025-08-04 09:38:53 +0200

    added build-time check to ensure that texture image file referenced by atlas data file exists

--- source/MonoGame.Extended.Content.Pipeline/TextureAtlases/TexturePackerProcessor.cs
+++ source/MonoGame.Extended.Content.Pipeline/TextureAtlases/TexturePackerProcessor.cs
@@ -3,6 +3,8 @@
 // See LICENSE file in the project root for full license information.
 
 using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
 using MonoGame.Extended.Content.TexturePacker;
 
 namespace MonoGame.Extended.Content.Pipeline.TextureAtlases;
@@ -12,6 +14,21 @@ public class TexturePackerProcessor : ContentProcessor<TexturePackerFileContent,
 {
     public override TexturePackerProcessorResult Process(TexturePackerFileContent input, ContentProcessorContext context)
     {
+        if (input.Meta.Image != null)
+        {
+            // Validates the texture exists and can be processed (fails build if missing)
+            var externalRef = new ExternalReference<Texture2DContent>(input.Meta.Image);
+            context.BuildAndLoadAsset<Texture2DContent, Texture2DContent>(externalRef, nameof(TextureProcessor));
+
+        }
+        else if (input.Meta.DataFormat == "monogame-extended")
+        {
+            foreach (var texture in input.Textures)
+            {
+                var externalRef = new ExternalReference<Texture2DContent>(texture.FileName);
+                context.BuildAndLoadAsset<Texture2DContent, Texture2DContent>(externalRef, nameof(TextureProcessor));
+            }
+        }
         return new TexturePackerProcessorResult(input);
     }
 }
