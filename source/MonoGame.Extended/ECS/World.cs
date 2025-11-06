d2fd8256c0d093a2ab94c4dbd2b1751e97f42792
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2025-11-05 23:54:57 -0500

    Maintenance update to align with coding guidelines

--- source/MonoGame.Extended/ECS/World.cs
+++ source/MonoGame.Extended/ECS/World.cs
@@ -9,6 +9,11 @@ namespace MonoGame.Extended.ECS
         private readonly Bag<IUpdateSystem> _updateSystems;
         private readonly Bag<IDrawSystem> _drawSystems;
 
+        internal EntityManager EntityManager { get; }
+        internal ComponentManager ComponentManager { get; }
+
+        public int EntityCount => EntityManager.ActiveCount;
+
         internal World()
         {
             _updateSystems = new Bag<IUpdateSystem>();
@@ -21,30 +26,33 @@ namespace MonoGame.Extended.ECS
         public override void Dispose()
         {
             foreach (var updateSystem in _updateSystems)
+            {
                 updateSystem.Dispose();
+            }
 
             foreach (var drawSystem in _drawSystems)
+            {
                 drawSystem.Dispose();
+            }
 
             _updateSystems.Clear();
             _drawSystems.Clear();
-            
+
             base.Dispose();
         }
-
-        internal EntityManager EntityManager { get; }
-        internal ComponentManager ComponentManager { get; }
-
-        public int EntityCount => EntityManager.ActiveCount;
 
         internal void RegisterSystem(ISystem system)
         {
             // ReSharper disable once ConvertIfStatementToSwitchStatement
             if (system is IUpdateSystem updateSystem)
+            {
                 _updateSystems.Add(updateSystem);
+            }
 
             if (system is IDrawSystem drawSystem)
+            {
                 _drawSystems.Add(drawSystem);
+            }
 
             system.Initialize(this);
         }
@@ -68,17 +76,21 @@ namespace MonoGame.Extended.ECS
         {
             EntityManager.Destroy(entity);
         }
-        
+
         public override void Update(GameTime gameTime)
         {
             foreach (var system in _updateSystems)
+            {
                 system.Update(gameTime);
+            }
         }
 
         public override void Draw(GameTime gameTime)
         {
             foreach (var system in _drawSystems)
+            {
                 system.Draw(gameTime);
+            }
         }
     }
 }
