48b73ec7a26d6a859b392a5e3711988a8ef42a2a
Christopher Whitley <103014489+AristurtleDev@users.noreply.github.com>
2026-01-18 00:58:59 -0500

    Fix using statements

--- source/MonoGame.Extended.Content.Pipeline/ContentItem.cs
+++ source/MonoGame.Extended.Content.Pipeline/ContentItem.cs
@@ -1,5 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Content.Pipeline.Processors;
+using MonoGame.Extended.Content.Tiled;
+using MonoGame.Extended.Content.Pipeline.Tiled;
 
 namespace MonoGame.Extended.Content.Pipeline
 {
