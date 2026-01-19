a8b845d068ad4d6bed0245fc9fd5ec898aa372cc
Uygar Yilmaz <uygar.raf@gmail.com>
2026-01-19 01:40:26 -0500

    Sanitize path in AddDependencyWithLogging

--- source/MonoGame.Extended.Content.Pipeline/ContentImporterContextExtensions.cs
+++ source/MonoGame.Extended.Content.Pipeline/ContentImporterContextExtensions.cs
@@ -8,8 +8,14 @@ public static class ContentImporterContextExtensions
     public static string AddDependencyWithLogging(this ContentImporterContext context, string filePath, string source)
     {
         source = Path.Combine(Path.GetDirectoryName(filePath), source);
-        ContentLogger.Log($"Adding dependency '{source}'");
-        context.AddDependency(source);
-        return source;
+
+        // Strip relative up and down traversal if exists.
+        // ie. we soemetimes have have paths like "X:\\src\\project\\Content\\Maps\\FooBar/../MapImages/blah"
+        // This will replace that with "X:\\src\\project\\Content\\Maps\\MapImages\\blah"
+        var sanitizedSource = Path.GetFullPath(source);
+
+        ContentLogger.Log($"Adding dependency '{sanitizedSource}'");
+        context.AddDependency(sanitizedSource);
+        return sanitizedSource;
     }
 }
