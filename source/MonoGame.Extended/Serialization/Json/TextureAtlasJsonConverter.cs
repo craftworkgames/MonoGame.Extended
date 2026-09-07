96d9a3cd9dce04b31f04b3777dfb06f872ffdf43
Uygar Yilmaz <uygar.raf@gmail.com>
2026-09-07 16:20:49 -0400

    Updated TextureAtlasJsonConverter to be Native AOT compatible.

--- source/MonoGame.Extended/Serialization/Json/TextureAtlasJsonConverter.cs
+++ source/MonoGame.Extended/Serialization/Json/TextureAtlasJsonConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -39,14 +40,53 @@ namespace MonoGame.Extended.Serialization.Json
             }
             else
             {
-                var metadata = JsonSerializer.Deserialize<InlineTextureAtlas>(ref reader, options);
+                if (reader.TokenType != JsonTokenType.StartObject)
+                {
+                    throw new JsonException($"Expected {nameof(JsonTokenType.StartObject)} token");
+                }
+
+                string textureProperty = string.Empty;
+                int regionWidth = 0;
+                int regionHeight = 0;
+
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndObject)
+                    {
+                        break;
+                    }
+
+                    if (reader.TokenType == JsonTokenType.PropertyName)
+                    {
+                        var propertyName = reader.GetString();
+                        reader.Read();
+
+                        if (string.Equals(propertyName, "texture", StringComparison.OrdinalIgnoreCase))
+                        {
+                            textureProperty = reader.GetString() ?? string.Empty;
+                        }
+                        else if (string.Equals(propertyName, "regionWidth", StringComparison.OrdinalIgnoreCase))
+                        {
+                            regionWidth = reader.GetInt32();
+                        }
+                        else if (string.Equals(propertyName, "regionHeight", StringComparison.OrdinalIgnoreCase))
+                        {
+                            regionHeight = reader.GetInt32();
+                        }
+                        else
+                        {
+                            Trace.TraceWarning($"Ignoring unexpected property: {propertyName}");
+                            reader.Skip();
+                        }
+                    }
+                }
 
                 // TODO: When we get to .NET Standard 2.1 it would be more robust to use
                 // [Path.GetRelativePath](https://docs.microsoft.com/en-us/dotnet/api/system.io.path.getrelativepath?view=netstandard-2.1)
-                var textureName = Path.GetFileNameWithoutExtension(metadata.Texture);
-                var textureDirectory = Path.GetDirectoryName(metadata.Texture);
+                var textureName = Path.GetFileNameWithoutExtension(textureProperty);
+                var textureDirectory = Path.GetDirectoryName(textureProperty);
                 var directory = Path.GetDirectoryName(_path);
-                var relativePath = Path.Combine(_contentManager.RootDirectory, directory, textureDirectory, textureName);
+                var relativePath = Path.Combine(_contentManager.RootDirectory, directory ?? string.Empty, textureDirectory ?? string.Empty, textureName);
                 var resolvedAssetName = Path.GetFullPath(relativePath);
                 Texture2D texture;
                 try
@@ -55,26 +95,24 @@ namespace MonoGame.Extended.Serialization.Json
                 }
                 catch (Exception ex)
                 {
-                    if (textureDirectory == null || textureDirectory == "")
+                    Trace.TraceWarning(
+                        $"Failed to load texture with {nameof(resolvedAssetName)}: {resolvedAssetName}. Attempting to load with {nameof(textureDirectory)}: {textureDirectory} and {nameof(textureName)}: {textureName}. Exception: {ex}");
+
+                    if (string.IsNullOrEmpty(textureDirectory))
+                    {
                         texture = _contentManager.Load<Texture2D>(textureName);
+                    }
                     else
+                    {
                         texture = _contentManager.Load<Texture2D>(textureDirectory + "/" + textureName);
+                    }
                 }
-                return Texture2DAtlas.Create(resolvedAssetName, texture, metadata.RegionWidth, metadata.RegionHeight);
+                return Texture2DAtlas.Create(resolvedAssetName, texture, regionWidth, regionHeight);
             }
         }
 
         /// <inheritdoc />
         public override void Write(Utf8JsonWriter writer, Texture2DAtlas value, JsonSerializerOptions options) { }
-
-
-        // ReSharper disable once ClassNeverInstantiated.Local
-        private class InlineTextureAtlas
-        {
-            public string Texture { get; set; }
-            public int RegionWidth { get; set; }
-            public int RegionHeight { get; set; }
-        }
 
         private string GetContentPath(string relativePath)
         {
